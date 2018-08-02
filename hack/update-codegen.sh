44f83ff9bd9b773159c2d630fcb6d1aef0b0728d
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2018-08-01 23:46:28 -0700

    hack: update build-go script

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -5,10 +5,8 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-# Location of the vendored code-generator package
-CODEGEN_PKG="../vendor/k8s.io/code-generator"
 
-${CODEGEN_PKG}/generate-groups.sh all \
+${SCRIPT_ROOT}/vendor/k8s.io/code-generator/generate-groups.sh all \
   github.com/openshift/machine-config-operator/pkg/generated github.com/openshift/machine-config-operator/pkg/apis \
   "machineconfiguration.openshift.io:v1" \
   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
