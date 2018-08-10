bfa878a9e808b1f8e2a447dc2c9f8364ea6a7112
Vikram Sreekumar <vsreekum@redhat.com>
2018-08-10 16:12:48 -0700

    server: add cmd for machine-config-server
        - add cmd for running machine config server in
          bootstrap and cluster modes.

---
+++ cmd/machine-config-server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+)
+
+const (
+	componentName = "machine-config-server"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   componentName,
+		Short: "Run Machine Config Server",
+		Long:  "",
+	}
+
+	rootOpts struct {
+		port  int
+		cert  string
+		key   string
+		debug bool
+	}
+)
+
+func init() {
+	rootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
+	rootCmd.PersistentFlags().IntVar(&rootOpts.port, "port", 45900, "port to serve ignition configs")
+	rootCmd.PersistentFlags().StringVar(&rootOpts.cert, "cert", "", "cert file for TLS")
+	rootCmd.PersistentFlags().StringVar(&rootOpts.key, "key", "", "key file for TLS")
+	rootCmd.PersistentFlags().BoolVar(&rootOpts.debug, "debug", false, "turns off TLS")
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		glog.Exitf("Error executing mcs: %v", err)
+	}
+}
