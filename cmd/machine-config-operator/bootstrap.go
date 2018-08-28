02bcdbbb25707ab10ca16ff03b370364683cf396
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2018-08-28 10:36:54 -0700

    mco: add bootstrap cmd

---
+++ cmd/machine-config-operator/bootstrap.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+
+	"github.com/openshift/machine-config-operator/pkg/operator"
+	"github.com/openshift/machine-config-operator/pkg/version"
+)
+
+var (
+	bootstrapCmd = &cobra.Command{
+		Use:   "bootstrap",
+		Short: "Machine Config Operator in bootstrap mode",
+		Long:  "",
+		Run:   runBootstrapCmd,
+	}
+
+	bootstrapOpts struct {
+		destinationDir string
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(bootstrapCmd)
+	bootstrapCmd.PersistentFlags().StringVar(&bootstrapOpts.destinationDir, "dest-dir", "", "The destination directory where MCO writes the manifests.")
+
+}
+
+func runBootstrapCmd(cmd *cobra.Command, args []string) {
+	flag.Set("logtostderr", "true")
+	flag.Parse()
+
+	// To help debugging, immediately log version
+	glog.Infof("Version: %+v", version.Version)
+
+	if bootstrapOpts.destinationDir == "" {
+		glog.Fatal("--dest-dir cannot be empty")
+	}
+
+	if err := operator.RenderBootstrap(bootstrapOpts.destinationDir); err != nil {
+		glog.Fatalf("error rendering bootstrap manifests: %v", err)
+	}
+}
