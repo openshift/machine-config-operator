320506db7e97e401a73243f0e4612f0618bb70e5
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2018-09-06 11:12:41 -0700

    pkg/version: update the Raw default to something the semver can parse
    this is requires so that go test ./... doesn't fail for pkg/operator/...
    as it imports the pkg/version which is not only correctly setup on build.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -10,7 +10,7 @@ import (
 var (
 	// Raw is the string representation of the version. This will be replaced
 	// with the calculated version at build time.
-	Raw = "was not built properly"
+	Raw = "v0.0.0-was-not-built-properly"
 
 	// Version is semver representation of the version.
 	Version = semver.MustParse(strings.TrimLeft(Raw, "v"))
