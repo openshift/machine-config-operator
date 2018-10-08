dc8a044f1abe65bc31f3fba7897a86bbcb586fd4
Steve Milner <smilner@redhat.com>
2018-10-08 11:49:38 -0400

    daemon/update_test.go: Initial example of mock testing
    Signed-off-by: Steve Milner <smilner@redhat.com>

---
+++ pkg/daemon/update_test.go
@@ -0,0 +1,62 @@
+package daemon
+
+import (
+	"fmt"
+	"testing"
+
+	mcfgv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
+	"github.com/openshift/machine-config-operator/pkg/generated/clientset/versioned/fake"
+	k8sfake "k8s.io/client-go/kubernetes/fake"
+)
+
+// TestUpdateOS verifies the return errors from attempting to update the OS follow expectations
+func TestUpdateOS(t *testing.T) {
+	// expectedError is the error we will use when expecting an error to return
+	expectedError := fmt.Errorf("broken")
+
+	// testClient is the NodeUpdaterClient mock instance that will front
+	// calls to update the host.
+	testClient := RpmOstreeClientMock{
+		GetBootedOSImageURLReturns: []GetBootedOSImageURLReturn{},
+		RunPivotReturns: []error{
+			// First run will return no error
+			nil,
+			// Second rrun will return our expected error
+			expectedError},
+	}
+
+	// Create a Daemon instance with mocked clients
+	d := Daemon{
+		name:              "nodeName",
+		OperatingSystem:   MachineConfigDaemonOSRHCOS,
+		NodeUpdaterClient: testClient,
+		loginClient:       nil, // set to nil as it will not be used within tests
+		client:            fake.NewSimpleClientset(),
+		kubeClient:        k8sfake.NewSimpleClientset(),
+		rootMount:         "/",
+		bootedOSImageURL:  "test",
+	}
+
+	// Set up machineconfigs to pass to updateOS.
+	mcfg := &mcfgv1.MachineConfig{}
+	// differentMcfg has a different OSImageURL so it will force Daemon.UpdateOS
+	// to trigger an update of the operatingsystem (as fronted by our testClient)
+	differentMcfg := &mcfgv1.MachineConfig{
+		Spec: mcfgv1.MachineConfigSpec{
+			OSImageURL: "somethingDifferent",
+		},
+	}
+
+	// Test when the machine configs match. No operation should occur
+	if err := d.updateOS(mcfg, mcfg); err != nil {
+		t.Errorf("Expected no error. Got %s.", err)
+	}
+	// When machine configs differ but pivot succeeds we should get no error.
+	if err := d.updateOS(mcfg, mcfg); err != nil {
+		t.Errorf("Expected no error. Got %s.", err)
+	}
+	// When machine configs differ but pivot fails we should get the expected error.
+	if err := d.updateOS(mcfg, differentMcfg); err == expectedError {
+		t.Error("Expected an error. Got none.")
+	}
+}
