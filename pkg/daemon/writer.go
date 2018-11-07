0bae29ef2bd46515a3049cc86383479542fd69ed
Ryan Phillips <rphillips@redhat.com>
2018-11-07 09:32:33 -0600

    daemon: add kubelet health and single writer

---
+++ pkg/daemon/writer.go
@@ -0,0 +1,129 @@
+package daemon
+
+import (
+	"fmt"
+
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/util/retry"
+)
+
+const (
+	// defaultWriterQueue the number of pending writes to queue
+	defaultWriterQueue = 25
+)
+
+// message wraps a client and responseChannel
+type message struct {
+	client          corev1.NodeInterface
+	node            string
+	annos           map[string]string
+	responseChannel chan error
+}
+
+// NodeWriter A single writer to Kubernetes to prevent race conditions
+type NodeWriter struct {
+	writer chan message
+}
+
+// NewNodeWriter Create a new NodeWriter
+func NewNodeWriter() *NodeWriter {
+	return &NodeWriter{
+		writer: make(chan message, defaultWriterQueue),
+	}
+}
+
+// Run reads from the writer channel and sets the node annotation. It will
+// return if the stop channel is closed. Intended to be run via a goroutine.
+func (nw *NodeWriter) Run(stop <-chan struct{}) {
+	for {
+		select {
+		case <-stop:
+			return
+		case msg := <-nw.writer:
+			msg.responseChannel <- setNodeAnnotations(msg.client, msg.node, msg.annos)
+		}
+	}
+}
+
+// SetUpdateDone Sets the state to UpdateDone.
+func (nw *NodeWriter) SetUpdateDone(client corev1.NodeInterface, node string, dcAnnotation string) error {
+	annos := map[string]string{
+		MachineConfigDaemonStateAnnotationKey: MachineConfigDaemonStateDone,
+		CurrentMachineConfigAnnotationKey:     dcAnnotation,
+	}
+	respChan := make(chan error, 1)
+	nw.writer <- message{
+		client:          client,
+		node:            node,
+		annos:           annos,
+		responseChannel: respChan,
+	}
+	return <-respChan
+}
+
+// SetUpdateWorking Sets the state to UpdateWorking.
+func (nw *NodeWriter) SetUpdateWorking(client corev1.NodeInterface, node string) error {
+	annos := map[string]string{
+		MachineConfigDaemonStateAnnotationKey: MachineConfigDaemonStateWorking,
+	}
+	respChan := make(chan error, 1)
+	nw.writer <- message{
+		client:          client,
+		node:            node,
+		annos:           annos,
+		responseChannel: respChan,
+	}
+	return <-respChan
+}
+
+// SetUpdateDegraded Sets the state to UpdateDegraded.
+func (nw *NodeWriter) SetUpdateDegraded(client corev1.NodeInterface, node string) error {
+	annos := map[string]string{
+		MachineConfigDaemonStateAnnotationKey: MachineConfigDaemonStateDegraded,
+	}
+	respChan := make(chan error, 1)
+	nw.writer <- message{
+		client:          client,
+		node:            node,
+		annos:           annos,
+		responseChannel: respChan,
+	}
+	return <-respChan
+}
+
+// updateNodeRetry calls f to update a node object in Kubernetes.
+// It will attempt to update the node by applying f to it up to DefaultBackoff
+// number of times.
+// f will be called each time since the node object will likely have changed if
+// a retry is necessary.
+func updateNodeRetry(client corev1.NodeInterface, node string, f func(*v1.Node)) error {
+	err := retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+		n, getErr := client.Get(node, metav1.GetOptions{})
+		if getErr != nil {
+			return getErr
+		}
+
+		// Call the node modifier.
+		f(n)
+
+		_, err := client.Update(n)
+		return err
+	})
+	if err != nil {
+		// may be conflict if max retries were hit
+		return fmt.Errorf("Unable to update node %q: %v", node, err)
+	}
+
+	return nil
+}
+
+// setConfig sets the given annotation key, value pair.
+func setNodeAnnotations(client corev1.NodeInterface, node string, m map[string]string) error {
+	return updateNodeRetry(client, node, func(node *v1.Node) {
+		for k, v := range m {
+			node.Annotations[k] = v
+		}
+	})
+}
