1d756390f1a046e20d9c17d09661281a134e8a1e
Ben Howard <ben.howard@redhat.com>
2018-11-28 09:07:56 -0700

    resourceread: use lib function for loading machine configs
    Use lib/resourceread for loading machineconfigs for the daemon. Added
    basic testing for load function.

--- lib/resourceread/machineconfig.go
+++ lib/resourceread/machineconfig.go
@@ -1,6 +1,9 @@
 package resourceread
 
 import (
+	"errors"
+	"fmt"
+
 	mcfgv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
@@ -17,13 +20,35 @@ func init() {
 	}
 }
 
-// ReadMachineConfigV1OrDie reads MachineConfig object from bytes. Panics on error.
+// ReadMachineConfigV1 reads raw MachineConfig object from bytes. Returns MachineConfig and error.
+func ReadMachineConfigV1(objBytes []byte) (*mcfgv1.MachineConfig, error) {
+	if objBytes == nil {
+		return nil, errors.New("invalid machine configuration")
+	}
+
+	m, err := runtime.Decode(mcfgCodecs.UniversalDecoder(mcfgv1.SchemeGroupVersion), objBytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode raw bytes to mcfgv1.SchemeGroupVersion: %v", err)
+	}
+	if m == nil {
+		return nil, fmt.Errorf("expected mcfgv1.SchemeGroupVersion but got nil")
+	}
+
+	mc, ok := m.(*mcfgv1.MachineConfig)
+	if !ok {
+		return nil, fmt.Errorf("expected *mcfvgv1.MachineConfig but found %T", m)
+	}
+
+	return mc, nil
+}
+
+// ReadMachineConfigV1OrDie reads raw  MachineConfig object from bytes. Panics on error.
 func ReadMachineConfigV1OrDie(objBytes []byte) *mcfgv1.MachineConfig {
-	requiredObj, err := runtime.Decode(mcfgCodecs.UniversalDecoder(mcfgv1.SchemeGroupVersion), objBytes)
+	mc, err := ReadMachineConfigV1(objBytes)
 	if err != nil {
 		panic(err)
 	}
-	return requiredObj.(*mcfgv1.MachineConfig)
+	return mc
 }
 
 // ReadMachineConfigPoolV1OrDie reads MachineConfigPool object from bytes. Panics on error.
