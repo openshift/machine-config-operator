d40c76c3788aa748ce5cd8033473c490d07383f7
Kirsten G <kikis.github@gmail.com>
2018-12-07 10:44:38 -0800

    update dependencies

---
+++ vendor/github.com/openshift/installer/pkg/types/openstack/machinepool.go
@@ -0,0 +1,20 @@
+package openstack
+
+// MachinePool stores the configuration for a machine pool installed
+// on OpenStack.
+type MachinePool struct {
+	// FlavorName defines the OpenStack Nova flavor.
+	// eg. m1.large
+	FlavorName string `json:"type"`
+}
+
+// Set sets the values from `required` to `a`.
+func (o *MachinePool) Set(required *MachinePool) {
+	if required == nil || o == nil {
+		return
+	}
+
+	if required.FlavorName != "" {
+		o.FlavorName = required.FlavorName
+	}
+}
