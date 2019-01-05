d0a7ae21b0e4d3174fd5ec41288204d4b0292019
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2019-01-04 16:21:00 -0800

    add healthz

--- pkg/server/api_test.go
+++ pkg/server/api_test.go
@@ -30,10 +30,132 @@ type scenario struct {
 func TestAPIHandler(t *testing.T) {
 	scenarios := []scenario{
 		{
-			name:    "get non-config path that does not exist",
-			request: httptest.NewRequest(http.MethodGet, "http://testrequest/does-not-exist", nil),
+			name:    "get config path that does not exist",
+			request: httptest.NewRequest(http.MethodGet, "http://testrequest/config/does-not-exist", nil),
 			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
-				return nil, nil
+				return new(ignv2_2types.Config), fmt.Errorf("not acceptable")
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusInternalServerError)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+		{
+			name:    "get config path that exists",
+			request: httptest.NewRequest(http.MethodGet, "http://testrequest/config/master", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusOK)
+				checkContentType(t, response, "application/json")
+				checkContentLength(t, response, 114)
+				checkBodyLength(t, response, 114)
+			},
+		},
+		{
+			name:    "head config path that exists",
+			request: httptest.NewRequest(http.MethodHead, "http://testrequest/config/master", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusOK)
+				checkContentType(t, response, "application/json")
+				checkContentLength(t, response, 114)
+				checkBodyLength(t, response, 0)
+			},
+		},
+		{
+			name:    "post config path that exists",
+			request: httptest.NewRequest(http.MethodPost, "http://testrequest/config/master", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusMethodNotAllowed)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			ms := &mockServer{
+				GetConfigFn: scenario.serverFunc,
+			}
+			handler := NewServerAPIHandler(ms)
+			handler.ServeHTTP(w, scenario.request)
+
+			resp := w.Result()
+			defer resp.Body.Close()
+			scenario.checkResponse(t, resp)
+		})
+	}
+}
+
+func TestHealthzHandler(t *testing.T) {
+	scenarios := []scenario{
+		{
+			name:    "get healthz",
+			request: httptest.NewRequest(http.MethodGet, "http://testrequest/healthz", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusNoContent)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+		{
+			name:    "head healthz",
+			request: httptest.NewRequest(http.MethodHead, "http://testrequest/healthz", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusNoContent)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+		{
+			name:    "post healthz",
+			request: httptest.NewRequest(http.MethodPost, "http://testrequest/healthz", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusMethodNotAllowed)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+	}
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			handler := &healthHandler{}
+			handler.ServeHTTP(w, scenario.request)
+
+			resp := w.Result()
+			defer resp.Body.Close()
+			scenario.checkResponse(t, resp)
+		})
+	}
+}
+
+func TestDefaultHandler(t *testing.T) {
+	scenarios := []scenario{
+		{
+			name:    "get root",
+			request: httptest.NewRequest(http.MethodGet, "http://testrequest/", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
 			},
 			checkResponse: func(t *testing.T, response *http.Response) {
 				checkStatus(t, response, http.StatusNotFound)
@@ -41,6 +163,46 @@ func TestAPIHandler(t *testing.T) {
 				checkBodyLength(t, response, 0)
 			},
 		},
+		{
+			name:    "head root",
+			request: httptest.NewRequest(http.MethodHead, "http://testrequest/", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusNotFound)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+		{
+			name:    "post root",
+			request: httptest.NewRequest(http.MethodPost, "http://testrequest/", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusMethodNotAllowed)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+	}
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			handler := &defaultHandler{}
+			handler.ServeHTTP(w, scenario.request)
+
+			resp := w.Result()
+			defer resp.Body.Close()
+			scenario.checkResponse(t, resp)
+		})
+	}
+}
+
+func TestAPIServer(t *testing.T) {
+	scenarios := []scenario{
 		{
 			name:    "get config path that does not exist",
 			request: httptest.NewRequest(http.MethodGet, "http://testrequest/config/does-not-exist", nil),
@@ -80,18 +242,6 @@ func TestAPIHandler(t *testing.T) {
 			},
 		},
 		{
-			name:    "post non-config path that does not exist",
-			request: httptest.NewRequest(http.MethodPost, "http://testrequest/post", nil),
-			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
-				return nil, nil
-			},
-			checkResponse: func(t *testing.T, response *http.Response) {
-				checkStatus(t, response, http.StatusMethodNotAllowed)
-				checkContentLength(t, response, 0)
-				checkBodyLength(t, response, 0)
-			},
-		},
-		{
 			name:    "post config path that exists",
 			request: httptest.NewRequest(http.MethodPost, "http://testrequest/config/master", nil),
 			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
@@ -103,16 +253,87 @@ func TestAPIHandler(t *testing.T) {
 				checkBodyLength(t, response, 0)
 			},
 		},
+		{
+			name:    "get healthz",
+			request: httptest.NewRequest(http.MethodGet, "http://testrequest/healthz", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusNoContent)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+		{
+			name:    "head healthz",
+			request: httptest.NewRequest(http.MethodHead, "http://testrequest/healthz", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusNoContent)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+		{
+			name:    "post healthz",
+			request: httptest.NewRequest(http.MethodPost, "http://testrequest/healthz", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusMethodNotAllowed)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+		{
+			name:    "get root",
+			request: httptest.NewRequest(http.MethodGet, "http://testrequest/", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusNotFound)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+		{
+			name:    "head root",
+			request: httptest.NewRequest(http.MethodHead, "http://testrequest/", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusNotFound)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
+		{
+			name:    "post root",
+			request: httptest.NewRequest(http.MethodPost, "http://testrequest/", nil),
+			serverFunc: func(poolRequest) (*ignv2_2types.Config, error) {
+				return new(ignv2_2types.Config), nil
+			},
+			checkResponse: func(t *testing.T, response *http.Response) {
+				checkStatus(t, response, http.StatusMethodNotAllowed)
+				checkContentLength(t, response, 0)
+				checkBodyLength(t, response, 0)
+			},
+		},
 	}
-
 	for _, scenario := range scenarios {
 		t.Run(scenario.name, func(t *testing.T) {
 			w := httptest.NewRecorder()
 			ms := &mockServer{
 				GetConfigFn: scenario.serverFunc,
 			}
-			handler := NewServerAPIHandler(ms)
-			handler.ServeHTTP(w, scenario.request)
+			server := NewAPIServer(NewServerAPIHandler(ms), 0, false, "", "")
+			server.handler.ServeHTTP(w, scenario.request)
 
 			resp := w.Result()
 			defer resp.Body.Close()
