openshift/machine-config-operator

commit 1d14b2a3eaee8065b3cd748caf6a39827c6afffa

MCD: rootPrefix now rootMount
rootPrefix has been renamed to rootMount and now is used as the
location to chroot. File operations no longer require joining
a global prefix due to the MCD running within the chroot.

Signed-off-by: Steve Milner <smilner@redhat.com>

pkg/daemon/update.go
@@ -89,13 +89,11 @@ func (dn *Daemon) deleteStaleData(oldConfig, newConfig *mcfgv1.MachineConfig) {
 	var path string
 	newFileSet := make(map[string]struct{})
 	for _, f := range newConfig.Spec.Config.Storage.Files {
-		path = filepath.Join(dn.prefix, f.Path)
-		newFileSet[path] = struct{}{}
+		newFileSet[f.Path] = struct{}{}
 	}
 
 	for _, f := range oldConfig.Spec.Config.Storage.Files {
-		path = filepath.Join(dn.prefix, f.Path)
-		if _, ok := newFileSet[path]; !ok {
+		if _, ok := newFileSet[f.Path]; !ok {
 			os.RemoveAll(path)
 		}
 	}
@@ -104,21 +102,21 @@ func (dn *Daemon) deleteStaleData(oldConfig, newConfig *mcfgv1.MachineConfig) {
 	newDropinSet := make(map[string]struct{})
 	for _, u := range newConfig.Spec.Config.Systemd.Units {
 		for j := range u.Dropins {
-			path = filepath.Join(dn.prefix, pathSystemd, u.Name+".d", u.Dropins[j].Name)
+			path = filepath.Join(pathSystemd, u.Name+".d", u.Dropins[j].Name)
 			newDropinSet[path] = struct{}{}
 		}
-		path = filepath.Join(dn.prefix, pathSystemd, u.Name)
+		path = filepath.Join(pathSystemd, u.Name)
 		newUnitSet[path] = struct{}{}
 	}
 
 	for _, u := range oldConfig.Spec.Config.Systemd.Units {
 		for j := range u.Dropins {
-			path = filepath.Join(dn.prefix, pathSystemd, u.Name+".d", u.Dropins[j].Name)
+			path = filepath.Join(pathSystemd, u.Name+".d", u.Dropins[j].Name)
 			if _, ok := newDropinSet[path]; !ok {
 				os.RemoveAll(path)
 			}
 		}
-		path = filepath.Join(dn.prefix, pathSystemd, u.Name)
+		path = filepath.Join(pathSystemd, u.Name)
 		if _, ok := newUnitSet[path]; !ok {
 			os.RemoveAll(path)
 		}
@@ -132,7 +130,7 @@ func (dn *Daemon) writeUnits(units []ignv2_2types.Unit) error {
 		// write the dropin to disk
 		for i := range u.Dropins {
 			glog.Infof("writing systemd unit dropin %q", u.Dropins[i].Name)
-			path = filepath.Join(dn.prefix, pathSystemd, u.Name+".d", u.Dropins[i].Name)
+			path = filepath.Join(pathSystemd, u.Name+".d", u.Dropins[i].Name)
 			if err := os.MkdirAll(filepath.Dir(path), os.FileMode(0655)); err != nil {
 				return fmt.Errorf("Failed to create directory %q: %v", filepath.Dir(path), err)
 			}
@@ -148,7 +146,7 @@ func (dn *Daemon) writeUnits(units []ignv2_2types.Unit) error {
 		}
 
 		glog.Infof("writing systemd unit %q", u.Name)
-		path = filepath.Join(dn.prefix, pathSystemd, u.Name)
+		path = filepath.Join(pathSystemd, u.Name)
 		if err := os.MkdirAll(filepath.Dir(path), os.FileMode(0655)); err != nil {
 			return fmt.Errorf("Failed to create directory %q: %v", filepath.Dir(path), err)
 		}
@@ -179,16 +177,14 @@ func (dn *Daemon) writeUnits(units []ignv2_2types.Unit) error {
 // it doesn't fetch remote files and expects a flattened config file.
 func (dn *Daemon) writeFiles(files []ignv2_2types.File) error {
 	for _, f := range files {
-		path := dn.prefix + f.Path
-
 		glog.Infof("Writing file %q", f.Path)
 		// create any required directories for the file
-		if err := os.MkdirAll(filepath.Dir(path), os.FileMode(0655)); err != nil {
+		if err := os.MkdirAll(filepath.Dir(f.Path), os.FileMode(0655)); err != nil {
 			return fmt.Errorf("Failed to create directory %q: %v", filepath.Dir(f.Path), err)
 		}
 
 		// create the file
-		file, err := os.Create(path)
+		file, err := os.Create(f.Path)
 		if err != nil {
 			return fmt.Errorf("Failed to create file %q: %v", f.Path, err)
 		}

cmd/machine-config-daemon/start.go
@@ -26,7 +26,7 @@ var (
 	startOpts struct {
 		kubeconfig string
 		nodeName   string
-		rootPrefix string
+		rootMount  string
 	}
 )
 
@@ -34,7 +34,7 @@ func init() {
 	rootCmd.AddCommand(startCmd)
 	startCmd.PersistentFlags().StringVar(&startOpts.kubeconfig, "kubeconfig", "", "Kubeconfig file to access a remote cluster (testing only)")
 	startCmd.PersistentFlags().StringVar(&startOpts.nodeName, "node-name", "", "kubernetes node name daemon is managing.")
-	startCmd.PersistentFlags().StringVar(&startOpts.rootPrefix, "root-prefix", "/rootfs", "where the nodes root filesystem is mounted, for the file stage.")
+	startCmd.PersistentFlags().StringVar(&startOpts.rootMount, "root-mount", "/rootfs", "where the nodes root filesystem is mounted for chroot and file manipulation.")
 }
 
 func runStartCmd(cmd *cobra.Command, args []string) {
@@ -60,15 +60,15 @@ func runStartCmd(cmd *cobra.Command, args []string) {
 	// Ensure that the rootMount exists
 	if _, err := os.Stat(startOpts.rootMount); err != nil {
 		if os.IsNotExist(err) {
-			glog.Fatalf("rootPrefix %s does not exist", startOpts.rootPrefix)
+			glog.Fatalf("rootMount %s does not exist", startOpts.rootMount)
 		}
-		glog.Fatalf("unable to verify rootPrefix %s exists: %s", startOpts.rootPrefix, err)
+		glog.Fatalf("unable to verify rootMount %s exists: %s", startOpts.rootMount, err)
 	}
 
 	// Chroot into the root file system
-	glog.Infof(`chrooting into rootPrefix`, startOpts.rootPrefix)
-	if err := syscall.Chroot(startOpts.rootPrefix); err != nil {
-		glog.Fatalf("unable to chroot to %s: %s", startOpts.rootPrefix, err)
+	glog.Infof(`chrooting into rootMount %s`, startOpts.rootMount)
+	if err := syscall.Chroot(startOpts.rootMount); err != nil {
+		glog.Fatalf("unable to chroot to %s: %s", startOpts.rootMount, err)
 	}
 
 	// move into / inside the chroot
@@ -77,11 +77,8 @@ func runStartCmd(cmd *cobra.Command, args []string) {
 		glog.Fatalf("unable to change directory to /: %s", err)
 	}
 
-	// Set the root prefix to "" since we are inside the rootfs chroot
-	startOpts.rootPrefix = ""
-
 	daemon, err := daemon.New(
-		startOpts.rootPrefix,
+		startOpts.rootMount,
 		startOpts.nodeName,
 		cb.ClientOrDie(componentName),
 		cb.KubeClientOrDie(componentName),

pkg/daemon/daemon.go
@@ -33,8 +33,8 @@ type Daemon struct {
 	// kubeClient allows interaction with Kubernetes, including the node we are running on.
 	kubeClient kubernetes.Interface
 
-	// prefix is where the root filesystem is mounted
-	prefix string
+	// rootMount is the location for the MCD to chroot in
+	rootMount string
 }
 
 const (
@@ -45,7 +45,7 @@ const (
 // New sets up the systemd and kubernetes connections needed to update the
 // machine.
 func New(
-	rootPrefix string,
+	rootMount string,
 	nodeName string,
 	client mcfgclientset.Interface,
 	kubeClient kubernetes.Interface,
@@ -64,7 +64,7 @@ func New(
 		loginClient: loginClient,
 		client:      client,
 		kubeClient:  kubeClient,
-		prefix:      rootPrefix,
+		rootMount:   rootMount,
 	}, nil
 }
 
@@ -181,7 +181,7 @@ func (dn *Daemon) validate() (bool, error) {
 func (dn *Daemon) checkUnits(units []ignv2_2types.Unit) bool {
 	for _, u := range units {
 		for j := range u.Dropins {
-			path := filepath.Join(dn.prefix, pathSystemd, u.Name+".d", u.Dropins[j].Name)
+			path := filepath.Join(pathSystemd, u.Name+".d", u.Dropins[j].Name)
 			if status := checkFileContents(path, u.Dropins[j].Contents); !status {
 				return false
 			}
@@ -191,7 +191,7 @@ func (dn *Daemon) checkUnits(units []ignv2_2types.Unit) bool {
 			continue
 		}
 
-		path := filepath.Join(dn.prefix, pathSystemd, u.Name)
+		path := filepath.Join(pathSystemd, u.Name)
 		if u.Mask {
 			link, err := filepath.EvalSymlinks(path)
 			if err != nil {
@@ -215,13 +215,12 @@ func (dn *Daemon) checkUnits(units []ignv2_2types.Unit) bool {
 // target config.
 func (dn *Daemon) checkFiles(files []ignv2_2types.File) bool {
 	for _, f := range files {
-		path := filepath.Join(dn.prefix, f.Path)
 		contents, err := dataurl.DecodeString(f.Contents.Source)
 		if err != nil {
 			glog.Errorf("couldn't parse file: %v", err)
 			return false
 		}
-		if status := checkFileContents(path, string(contents.Data)); !status {
+		if status := checkFileContents(f.Path, string(contents.Data)); !status {
 			return false
 		}
 	}

commit 8da32e339555479e791bc8bc1e7f01243b581372

daemon: fix deletion of stale config files
Minor regression from 1d14b2a. We weren't passing the right variable to
`RemoveAll()`.

Closes: #268

pkg/daemon/update.go
@@ -281,7 +281,7 @@ func (dn *Daemon) deleteStaleData(oldConfig, newConfig *mcfgv1.MachineConfig) {
 	glog.V(2).Info("Removing stale config storage files")
 	for _, f := range oldConfig.Spec.Config.Storage.Files {
 		if _, ok := newFileSet[f.Path]; !ok {
-			dn.fileSystemClient.RemoveAll(path)
+			dn.fileSystemClient.RemoveAll(f.Path)
 		}
 	}
 