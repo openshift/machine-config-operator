6b08f68185b98f014b425fd95169f620730604ff
Colin Walters <walters@verbum.org>
2019-01-14 15:11:19 +0000

    daemon: Log pending/desired/config names very early
    Currently if we fail to check the node state we don't end up
    logging which config names we're using.  Let's do so as it's
    rather crucial to understanding which MCs might have caused
    a degredation.

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -389,11 +389,15 @@ func (dn *Daemon) getStateAndConfigs(pendingConfigName string) (*stateAndConfigs
 	var desiredConfig *mcfgv1.MachineConfig
 	if currentConfigName == desiredConfigName {
 		desiredConfig = currentConfig
+		glog.Infof("Current+desired config: %s", currentConfigName)
 	} else {
 		desiredConfig, err = getMachineConfig(dn.client.MachineconfigurationV1().MachineConfigs(), desiredConfigName)
 		if err != nil {
 			return nil, err
 		}
+
+		glog.Infof("Current config: %s", currentConfigName)
+		glog.Infof("Desired config: %s", desiredConfigName)
 	}
 
 	var pendingConfig *mcfgv1.MachineConfig
@@ -406,6 +410,8 @@ func (dn *Daemon) getStateAndConfigs(pendingConfigName string) (*stateAndConfigs
 		if err != nil {
 			return nil, err
 		}
+
+		glog.Infof("Pending config: %s", pendingConfigName)
 	}
 
 	return &stateAndConfigs{
