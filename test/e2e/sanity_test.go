4921826eb9bbb7097fbc505f56bb229ec27b7a12
Colin Walters <walters@verbum.org>
2019-01-16 23:08:09 +0000

    test/e2e: Validate that no nodes went degraded
    I think we have this in CI but we're not noticing it.
    If it's happening we need to fix it.
    
    Ref: https://github.com/openshift/machine-config-operator/issues/301

--- test/e2e/sanity_test.go
+++ test/e2e/sanity_test.go
@@ -2,10 +2,16 @@ package e2e_test
 
 import (
 	"testing"
+	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/informers"
+	"k8s.io/apimachinery/pkg/labels"
 
 	"github.com/openshift/machine-config-operator/cmd/common"
+	"github.com/openshift/machine-config-operator/pkg/daemon"
 )
 
 // Test case for https://github.com/openshift/machine-config-operator/pull/288/commits/44d5c5215b5450fca32806f796b50a3372daddc2
@@ -24,5 +30,46 @@ func TestOperatorLabel(t *testing.T) {
 	osSelector := d.Spec.Template.Spec.NodeSelector["beta.kubernetes.io/os"]
 	if osSelector != "linux" {
 		t.Errorf("Expected node selector 'linux', not '%s'", osSelector)
+	}
+}
+
+func TestNoDegraded(t *testing.T) {
+	cb, err := common.NewClientBuilder("")
+	if err != nil{
+		t.Errorf("%#v", err)
+	}
+	k := cb.KubeClientOrDie("sanity-test")
+
+	kubeSharedInformer := informers.NewSharedInformerFactory(k, 20 * time.Minute)
+	nodeInformer := kubeSharedInformer.Core().V1().Nodes()
+	nodeLister := nodeInformer.Lister()
+	nodeListerSynced := nodeInformer.Informer().HasSynced
+
+	stopCh := make(chan struct{})
+	kubeSharedInformer.Start(stopCh)
+	cache.WaitForCacheSync(stopCh, nodeListerSynced)
+
+	nodes, err := nodeLister.List(labels.Everything())
+	if err != nil {
+		t.Errorf("listing nodes: %v", err)
+	}
+
+	var degraded []*corev1.Node
+	for _, node := range nodes {
+		if node.Annotations == nil {
+			continue
+		}
+		dstate, ok := node.Annotations[daemon.MachineConfigDaemonStateAnnotationKey]
+		if !ok || dstate == "" {
+			continue
+		}
+
+		if dstate == daemon.MachineConfigDaemonStateDegraded {
+			degraded = append(degraded, node)
+		}
+	}
+
+	if len(degraded) > 0 {
+		t.Errorf("%d degraded nodes found", len(degraded))
 	}
 }
