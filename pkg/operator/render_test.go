79837d372e440bd37430b1152b4420aa1d3e5754
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2019-02-01 13:38:07 -0800

    operator: use infra and network objects to create controllerconfig
    infra object provides the etcd_discover_domain [1] and api_server_url, while
    the network object provides the service_network [3] used to calculate the cluster_dns_ip.
    
    currently the network objects's `ServiceNetwork` list only suports single service network [3] and therefore it
    is safe to only use the first element.
    
    [1]: https://github.com/openshift/api/blob/4912e102a00fa8bb4bf27f83808dce6ef1b6887d/config/v1/types_infrastructure.go#L42
    [2]: https://github.com/openshift/api/blob/4912e102a00fa8bb4bf27f83808dce6ef1b6887d/config/v1/types_infrastructure.go#L47
    [3]: https://github.com/openshift/api/blob/4912e102a00fa8bb4bf27f83808dce6ef1b6887d/config/v1/types_network.go#L32

--- pkg/operator/render_test.go
+++ pkg/operator/render_test.go
@@ -3,9 +3,6 @@ package operator
 import (
 	"fmt"
 	"testing"
-
-	"github.com/ghodss/yaml"
-	installertypes "github.com/openshift/installer/pkg/types"
 )
 
 func TestClusterDNSIP(t *testing.T) {
@@ -36,69 +33,5 @@ func TestClusterDNSIP(t *testing.T) {
 				t.Fatalf("%s failed: got = %s want = %s", desc, gotIP, test.Output)
 			}
 		})
-	}
-}
-
-var (
-	testInstallConfig = []byte(`
-sshKey: ssh-rsa AAAA....
-baseDomain: tt.testing
-clusterID: 2d149e46-90ee-3436-018a-1b02f6864006
-machines:
-- name: master
-  platform:
-    libvirt:
-      qcowImagePath: /path/rhcos-qemu.qcow2
-  replicas: 1
-- name: worker
-  platform:
-    libvirt:
-      qcowImagePath: /path/rhcos-qemu.qcow2
-  replicas: 2
-metadata:
-  creationTimestamp: null
-  name: test-0
-networking:
-  podCIDR: 10.2.0.0/16
-  serviceCIDR: 10.3.0.0/16
-  type: flannel
-platform:
-  libvirt:
-    URI: qemu:///system
-    masterIPs: null
-    network:
-      if: tt0
-      ipRange: 192.168.124.0/24
-      name: tectonic
-      resolver: 8.8.8.8
-pullSecret: ''
-`)
-)
-
-func TestDiscoverMCOConfig(t *testing.T) {
-	icgetter := func() (installertypes.InstallConfig, error) {
-		var ic installertypes.InstallConfig
-		if err := yaml.Unmarshal(testInstallConfig, &ic); err != nil {
-			return ic, err
-		}
-		return ic, nil
-	}
-
-	mco, err := discoverMCOConfig(icgetter)
-	if err != nil {
-		t.Fatalf("couldn't parse installconfig: %v", err)
-	}
-
-	if got, want := mco.Spec.ClusterDNSIP, "10.3.0.10"; got != want {
-		t.Fatalf("mismatch got = %v want = %v", got, want)
-	}
-	if got, want := mco.Spec.ClusterName, "test-0"; got != want {
-		t.Fatalf("mismatch got = %v want = %v", got, want)
-	}
-	if got, want := mco.Spec.Platform, "libvirt"; got != want {
-		t.Fatalf("mismatch got = %v want = %v", got, want)
-	}
-	if got, want := mco.Spec.BaseDomain, "tt.testing"; got != want {
-		t.Fatalf("mismatch got = %v want = %v", got, want)
 	}
 }
