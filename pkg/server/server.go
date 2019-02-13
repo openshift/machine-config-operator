2fcdd0e6c55119e3a8734d8f5b692678a5551046
Colin Walters <walters@verbum.org>
2019-02-13 22:50:52 +0000

    server: Implement "early pivot" before the MCD comes up
    We landed a lot of code to have the MCD call out to `pivot.service`;
    this builds on that to implement an "early pivot" model where
    we do the OS update before the node even joins the cluster.
    
    This should result in less disruption, though debuggability is weaker.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -16,6 +16,9 @@ const (
 	// of the KubeConfig file on the machine.
 	defaultMachineKubeConfPath = "/etc/kubernetes/kubeconfig"
 
+	// From https://github.com/openshift/pivot/pull/25/commits/c77788a35d7ee4058d1410e89e6c7937bca89f6c#diff-04c6e90faac2675aa89e2176d2eec7d8R44
+	pivotRebootNeeded = "/run/pivot/reboot-needed"
+
 	// defaultFileSystem defines the default file system to be
 	// used for writing the ignition files created by the
 	// server.
@@ -34,14 +37,49 @@ type Server interface {
 	GetConfig(poolRequest) (*ignv2_2types.Config, error)
 }
 
-func getAppenders(cr poolRequest, currMachineConfig string, f kubeconfigFunc) []appenderFunc {
+func getAppenders(cr poolRequest, currMachineConfig string, f kubeconfigFunc, osimageurl string) []appenderFunc {
 	appenders := []appenderFunc{
 		// append machine annotations file.
 		func(config *ignv2_2types.Config) error { return appendNodeAnnotations(config, currMachineConfig) },
+		// append pivot
+		func(config *ignv2_2types.Config) error { return appendInitialPivot(config, osimageurl) },
 		// append kubeconfig.
 		func(config *ignv2_2types.Config) error { return appendKubeConfig(config, f) },
 	}
 	return appenders
+}
+
+// Golang :cry:
+func boolToPtr(b bool) *bool {
+	return &b
+}
+
+func appendInitialPivot(conf *ignv2_2types.Config, osimageurl string) error {
+	if osimageurl == "" {
+		return nil
+	}
+
+	// Tell pivot.service to pivot early
+	appendFileToIgnition(conf, daemonconsts.EtcPivotFile, osimageurl + "\n")
+	// Awful hack to create a file in /run
+	// https://github.com/openshift/machine-config-operator/pull/363#issuecomment-463397373
+	// "So one gotcha here is that Ignition will actually write `/run/pivot/image-pullspec` to the filesystem rather than the `/run` tmpfs"
+	if len(conf.Systemd.Units) == 0 {
+		conf.Systemd.Units = make([]ignv2_2types.Unit, 0)
+	}
+	unit := ignv2_2types.Unit {
+		Name: "mcd-write-pivot-reboot.service",
+		Enabled: boolToPtr(true),
+		Contents: `[Unit]
+Before=pivot.service
+ConditionFirstBoot=true
+[Service]
+ExecStart=/bin/sh -c 'mkdir /run/pivot && touch /run/pivot/reboot-needed'
+[Install]
+WantedBy=multi-user.target
+`,}
+	conf.Systemd.Units = append(conf.Systemd.Units, unit)
+	return nil
 }
 
 func appendKubeConfig(conf *ignv2_2types.Config, f kubeconfigFunc) error {
