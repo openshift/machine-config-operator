53dabf1e70aa001030b6198bdc0cb8113b29b2e1
Antonio Murdaca <runcom@linux.com>
2019-02-15 22:32:32 +0100

    pkg/operator: link related objects to clusteroperator
    This patch populates the RelatedObjects field of the MCO ClusterOperator
    so openshift/must-gather can consume it. We're just exposing our own
    namespace for the tool to scrape.
    
    Signed-off-by: Antonio Murdaca <runcom@linux.com>

--- pkg/operator/status.go
+++ pkg/operator/status.go
@@ -150,6 +150,10 @@ func (optr *Operator) initializeClusterOperator() (*configv1.ClusterOperator, er
 	cov1helpers.SetStatusCondition(&co.Status.Conditions, configv1.ClusterOperatorStatusCondition{Type: configv1.OperatorAvailable, Status: configv1.ConditionFalse})
 	cov1helpers.SetStatusCondition(&co.Status.Conditions, configv1.ClusterOperatorStatusCondition{Type: configv1.OperatorProgressing, Status: configv1.ConditionFalse})
 	cov1helpers.SetStatusCondition(&co.Status.Conditions, configv1.ClusterOperatorStatusCondition{Type: configv1.OperatorFailing, Status: configv1.ConditionFalse})
+	// RelatedObjects are consumed by https://github.com/openshift/must-gather
+	co.Status.RelatedObjects = []configv1.ObjectReference{
+		{Resource: "namespaces", Name: "openshift-machine-config-operator"},
+	}
 	return optr.configClient.ConfigV1().ClusterOperators().UpdateStatus(co)
 }
 
