023f810cdcd7f1a469712d696d28b3c038a24b61
Colin Walters <walters@verbum.org>
2019-02-25 21:21:16 +0000

    daemon: Only print status if os == RHCOS
    Don't crash on non-rpm-ostree based systems.

--- cmd/machine-config-daemon/start.go
+++ cmd/machine-config-daemon/start.go
@@ -154,12 +154,6 @@ func runStartCmd(cmd *cobra.Command, args []string) {
 	}
 
 	if startOpts.onceFrom == "" {
-		status, err := dn.NodeUpdaterClient.GetStatus()
-		if err != nil {
-			glog.Fatalf("unable to get rpm-ostree status: %s", err)
-		}
-		glog.Info(status)
-
 		err = dn.CheckStateOnBoot()
 		if err != nil {
 			dn.EnterDegradedState(errors.Wrapf(err, "Checking initial state"))
