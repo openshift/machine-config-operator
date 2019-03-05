c65c2360e3c93faeaf4c7b1efd221f1763532a29
Ryan Phillips <ryan@trolocsis.com>
2019-03-04 19:10:29 -0600

    kubelet-config: prevent changing the ConfigMap and Secret Changing Strategy
    ref: https://github.com/openshift/machine-config-operator/issues/520

--- pkg/controller/kubelet-config/kubelet_config_controller.go
+++ pkg/controller/kubelet-config/kubelet_config_controller.go
@@ -63,6 +63,10 @@ var blacklistKubeletConfigurationFields = []string{
 	"CgroupDriver",
 	"ClusterDNS",
 	"ClusterDomain",
+	// Bugfix to force cache based configmap and secret watches. This should be
+	// removed with Kubernetes 1.14.
+	//   https://github.com/kubernetes/kubernetes/issues/74412
+	"ConfigMapAndSecretChangeDetectionStrategy",
 	"RuntimeRequestTimeout",
 	"StaticPodPath",
 }
