d3fc7f9e55240cbe20d61e08788214369f631f76
Colin Walters <walters@verbum.org>
2019-03-19 22:04:49 +0000

    switch to internal job

--- hack/cluster-cvo-push.sh
+++ hack/cluster-cvo-push.sh
@@ -18,6 +18,8 @@ NS=openshift-machine-config-operator
 COMPONENTS="operator controller server daemon"
 podman=${podman:-podman}
 
+internal_registry="image-registry.openshift-image-registry.svc:5000"
+internal_registry_ns="${internal_registry}/${NS}"
 registry=$(oc get -n openshift-image-registry -o json route/image-registry | jq -r ".spec.host")
 curl -k --head https://"${registry}" >/dev/null
 registry_ns="${registry}/${NS}"
@@ -38,11 +40,28 @@ for c in $COMPONENTS; do
     imgbuilds[$c]="${digest}"
 done
 
-dev_img=${registry_ns}/mcodev:latest
-oc adm release new --from-release ${running_version} --to-image=${dev_img} --insecure \
-   machine-config-operator=${registry_ns}/machine-config-operator:${VERSION} \
-   machine-config-controller=${registry_ns}/machine-config-controller:${VERSION} \
-   machine-config-server=${registry_ns}/machine-config-server:${VERSION} \
-   machine-config-daemon=${registry_ns}/machine-config-daemon:${VERSION}
+updateimg=${internal_registry_ns}/mcodev:latest
+cmd="oc adm release new --from-release ${running_version} --to-image=${updateimg} \
+   machine-config-operator=${internal_registry_ns}/machine-config-operator:${VERSION} \
+   machine-config-controller=${internal_registry_ns}/machine-config-controller:${VERSION} \
+   machine-config-server=${internal_registry_ns}/machine-config-server:${VERSION} \
+   machine-config-daemon=${internal_registry_ns}/machine-config-daemon:${VERSION} && \
+   oc adm upgrade --to-image ${updateimg} --force"
+cli_image=$(oc -n openshift-cluster-version rsh deploy/cluster-version-operator cluster-version-operator image cli)
+oc -n openshift-cluster-version create -f - <<EOF
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: mco-build-update-image
+spec:
+  template:
+    spec:
+      containers:
+      - name: mco-build-update-image
+        image: ${cli_image}
+        command: ["/bin/sh", "-c", "${cmd}"]
+      restartPolicy: Never
+EOF
 
-oc adm upgrade --to-image "${dev_img}" --force
+
+
