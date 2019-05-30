d1b91ab558abc92f2b1ab8580aadc6712048e7a2
Miloslav Trmač <mitr@redhat.com>
2019-05-30 16:05:38 +0200

    Update registries.conf to v2
    Update both the templates and updateRegistriesConfig.
    
    Also add tests for updateRegistriesConfig; the existing tests
    only test that the controller makes the right kinds of API calls, but does not
    check the content at all.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

---
+++ pkg/controller/container-runtime-config/helpers_test.go
@@ -0,0 +1,108 @@
+package containerruntimeconfig
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/BurntSushi/toml"
+	"github.com/containers/image/pkg/sysregistriesv2"
+	"github.com/containers/image/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/util/diff"
+)
+
+func TestUpdateRegistriesConfig(t *testing.T) {
+	templateConfig := sysregistriesv2.V2RegistriesConf{ // This matches templates/*/01-*-container-runtime/_base/files/container-registries.yaml
+		UnqualifiedSearchRegistries: []string{"registry.access.redhat.com", "docker.io"},
+	}
+	buf := bytes.Buffer{}
+	if err := toml.NewEncoder(&buf).Encode(templateConfig); err != nil {
+		t.Fatalf("Error marshaling templateConfig")
+	}
+	templateBytes := buf.Bytes()
+
+	tests := []struct {
+		name              string
+		insecure, blocked []string
+		want              sysregistriesv2.V2RegistriesConf
+	}{
+		{
+			name: "unchanged",
+			want: templateConfig,
+		},
+		{
+			name:     "insecure+blocked",
+			insecure: []string{"registry.access.redhat.com", "insecure.com", "common.com"},
+			blocked:  []string{"blocked.com", "common.com", "docker.io"},
+			want: sysregistriesv2.V2RegistriesConf{
+				UnqualifiedSearchRegistries: []string{"registry.access.redhat.com", "docker.io"},
+				Registries: []sysregistriesv2.Registry{
+					{
+						Endpoint: sysregistriesv2.Endpoint{
+							Location: "registry.access.redhat.com",
+							Insecure: true,
+						},
+					},
+					{
+						Endpoint: sysregistriesv2.Endpoint{
+							Location: "insecure.com",
+							Insecure: true,
+						},
+					},
+					{
+						Endpoint: sysregistriesv2.Endpoint{
+							Location: "common.com",
+							Insecure: true,
+						},
+						Blocked: true,
+					},
+					{
+						Endpoint: sysregistriesv2.Endpoint{
+							Location: "blocked.com",
+						},
+						Blocked: true,
+					},
+					{
+						Endpoint: sysregistriesv2.Endpoint{
+							Location: "docker.io",
+						},
+						Blocked: true,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := updateRegistriesConfig(templateBytes, tt.insecure, tt.blocked)
+			if err != nil {
+				t.Errorf("updateRegistriesConfig() error = %v", err)
+				return
+			}
+			gotConf := sysregistriesv2.V2RegistriesConf{}
+			if _, err := toml.Decode(string(got), &gotConf); err != nil {
+				t.Errorf("error unmarshalling result: %v", err)
+				return
+			}
+			// This assumes a specific order of Registries entries, which does not actually matter; ideally, this would
+			// sort the two arrays before comparing, but right now hard-coding the order works well enough.
+			if !reflect.DeepEqual(gotConf, tt.want) {
+				t.Errorf("updateRegistriesConfig() Diff:\n %s", diff.ObjectGoPrintDiff(tt.want, gotConf))
+			}
+			// Ensure that the generated configuration is actually valid.
+			registriesConf, err := ioutil.TempFile("", "registries.conf")
+			require.NoError(t, err)
+			_, err = registriesConf.Write(got)
+			require.NoError(t, err)
+			defer os.Remove(registriesConf.Name())
+			_, err = sysregistriesv2.GetRegistries(&types.SystemContext{
+				SystemRegistriesConfPath: registriesConf.Name(),
+			})
+			assert.NoError(t, err)
+		})
+	}
+}
