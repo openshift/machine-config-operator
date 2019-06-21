86196aa19a96101439ada97e9d224036ee88455a
Colin Walters <walters@verbum.org>
2019-06-21 19:18:54 +0000

    daemon: Move "keep container" logic into daemon too
    This way `pivot` isn't executing `podman` at all.

--- pkg/daemon/rpm-ostree_test.go
+++ pkg/daemon/rpm-ostree_test.go
@@ -42,7 +42,7 @@ func (r RpmOstreeClientMock) RunPivot(string) error {
 }
 
 // PullAndRebase is a mock
-func (r RpmOstreeClientMock) PullAndRebase(string) (string, bool, error) {
+func (r RpmOstreeClientMock) PullAndRebase(string, bool) (string, bool, error) {
 	return "", false, nil
 }
 
