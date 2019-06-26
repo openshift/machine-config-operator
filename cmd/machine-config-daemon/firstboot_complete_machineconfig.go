fc36d502b88b84b3cfc3a30c48d6e20213cc243b
Colin Walters <walters@verbum.org>
2019-06-26 13:32:43 +0000

    daemon: Add a m-c-d-firstboot.service that handles encapsulated MC
    The saga of adding "firstboot MachineConfig" in
    https://github.com/openshift/machine-config-operator/issues/798
    is getting closer.  This is a small amount of code that builds on
    a lot of prep work that landed to add a systemd service + entrypoint
    in the MCD that reads the
    `/etc/ignition-machine-config-encapsulated.json` file.  You
    could think of this a lot like a variant of "once-from".
    
    However, it doesn't run by default right now because the MCS still
    serves `/etc/pivot/image-pullspec`, and this gives us a way to
    "ratchet" the change as we need this new MCD code to land in RHCOS
    before we can use it.

---
+++ cmd/machine-config-daemon/firstboot_complete_machineconfig.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	daemon "github.com/openshift/machine-config-operator/pkg/daemon"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+var firstbootCompleteMachineconfig = &cobra.Command{
+	Use:                   "firstboot-complete-machineconfig",
+	DisableFlagsInUseLine: true,
+	Short:                 "Complete the host's initial boot into a MachineConfig",
+	Args:                  cobra.MaximumNArgs(0),
+	Run:                   executeFirstbootCompleteMachineConfig,
+}
+
+// init executes upon import
+func init() {
+	rootCmd.AddCommand(firstbootCompleteMachineconfig)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+}
+
+func runFirstBootCompleteMachineConfig(_ *cobra.Command, _ []string) error {
+	flag.Set("logtostderr", "true")
+	flag.Parse()
+
+	exitCh := make(chan error)
+	defer close(exitCh)
+
+	dn, err := daemon.New(daemon.NewNodeUpdaterClient(), exitCh)
+	if err != nil {
+		return err
+	}
+
+	err = dn.RunFirstbootCompleteMachineconfig()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func executeFirstbootCompleteMachineConfig(cmd *cobra.Command, args []string) {
+	err := runFirstBootCompleteMachineConfig(cmd, args)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+}
