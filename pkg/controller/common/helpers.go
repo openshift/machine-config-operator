80f7838e1666f6de60a0c6e7412bc78ed58cef54
Kirsten G <kikis.github@gmail.com>
2019-07-15 18:53:04 -0700

    mcc/mcd: add exception to validation for empty ign configs
    Wrap ignition validation in helper function that adds exception
    for empty ign configs.  Use helper validation function in update.go
    and render_controller.go
    
    Closes: #888

--- pkg/controller/common/helpers.go
+++ pkg/controller/common/helpers.go
@@ -2,9 +2,12 @@ package common
 
 import (
 	"io/ioutil"
+	"reflect"
 
 	igntypes "github.com/coreos/ignition/config/v2_2/types"
+	validate "github.com/coreos/ignition/config/validate"
 	"github.com/golang/glog"
+	errors "github.com/pkg/errors"
 )
 
 // NewIgnConfig returns an empty ignition config with version set as latest version
@@ -21,4 +24,20 @@ func WriteTerminationError(err error) {
 	msg := err.Error()
 	ioutil.WriteFile("/dev/termination-log", []byte(msg), 0644)
 	glog.Fatal(msg)
+}
+
+// ValidateIgnition wraps the underlying Ignition validation, but explicitly supports
+// a completely empty Ignition config as valid.  This is because we
+// want to allow MachineConfig objects which just have e.g. KernelArguments
+// set, but no Ignition config.
+// Returns nil if the config is valid (per above) or an error containing a Report otherwise.
+func ValidateIgnition(cfg igntypes.Config) error {
+	// only validate if Ignition Config is not empty
+	if reflect.DeepEqual(igntypes.Config{}, cfg) {
+		return nil
+	}
+	if report := validate.ValidateWithoutSource(reflect.ValueOf(cfg)); report.IsFatal() {
+		return errors.Errorf("invalid Ignition config found: %v", report)
+	}
+	return nil
 }
