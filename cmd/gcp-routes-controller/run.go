728f63a1d9aa81c4866215d2b4a1d849abacd622
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2019-08-01 17:53:57 -0700

    cmd: add gcp-routes-controllers to manage the routes service to prevent blackholes
    turn a service on/off based on the status of the health check provided.
    
    ** Background
    
    On GCP, network load balancing requires modification of the local machine's routes to include the forwarded_ips of the LB rules.
    Therefore, for a VM in the backend pool, the traffic to the LB never leaves the box (regardless of the health checks...)
    for more info: https://cloud.google.com/load-balancing/docs/internal/setting-up-internal#test-from-backend-vms
    
    This esp. affects the OpenShift control-plane as the clients like kubelet, openshift-networking (SDN) communicate with the apiserver using the LB.. and therefore can only communicate if the
    local apiservers is running. This is very fragile during roll-out of the apiserver pods as any interminent faliure to start the local apiserver pod means that kubelet on the machine can no longer
    reach any api to receive new pods for recovery to new version of the apiserver. A lot of the early users and CI is seeing these kinds of failures.
    
    ** What
    
    With https://gitlab.cee.redhat.com/coreos/redhat-coreos/merge_requests/481, the RHCOS's gcp-routes.service now cleans up any routes that were added for forwarded_ips when stopped, so to recover the blackhole, a user can logon to the machine
    and stop the service long enough for progress to happen and then start the service again to get traffic again..
    
    gcp-routes-controller performs health checks on the local apiserver so that,
    - When the apiserver is known to be failing, the momentum to mark failure is large enough to not interfer with LB's halthchecks and trigger only in require's intervention cases, stop the gcp-routes.service to allow clients to communicate to other apiservers over the LB
    - When the apiserver is running (succeeding), the momentum to mark sucess to small enough that the Lb's health checks will adde the instance to it's backend, it starts the service.
    
    ** What it solves
    
    It solves 2 cases for GCP:
    
    1) Improves the reliability of the apiserver roll-outs on GCP, making sure the kubelets have a chance to receive new versions that can unstick roll-outs
    
    2) Improve the installation of GCP.
       Currently, the installer during bootstrapping only adds the bootstrap-host to the LB backend, as adding the control-plane makes the kubelets on those nodes incapaable to reaching out the bootstrap-control-plane on the bootstrap-host
       and switches the backends to control-plane during bootstrap destroy, making destroy bootstrap a required step to complete the bootstrapping and also creating a period of time where there are no active backends.
       On other platforms, installer keeps all nodes in the LB backend and after bootstrap-complete the bootstap-host is remove from the backends by the LB itself and technically the bootstrap destroy step is not required.
    
       This helps bring the GCP flow at par with others as the gcp-route-controller on the control-plane hosts will run the gcp-routes when the apiserver is running locally and the installer can keep all machines in the backend.

---
+++ cmd/gcp-routes-controller/run.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"sync"
+	"syscall"
+	"time"
+
+	health "github.com/InVisionApp/go-health"
+	"github.com/InVisionApp/go-health/checkers"
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+
+	"github.com/openshift/machine-config-operator/pkg/version"
+)
+
+var (
+	runCmd = &cobra.Command{
+		Use:   "run",
+		Short: "Runs the gcp-routes-controller",
+		Long:  "",
+		RunE:  runRunCmd,
+	}
+
+	runOpts struct {
+		gcpRoutesService string
+		rootMount        string
+
+		healthCheckURL string
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+	runCmd.PersistentFlags().StringVar(&runOpts.gcpRoutesService, "gcp-routes-service", "gcp-routes.service", "The name for the service controlling gcp routes on host")
+	runCmd.PersistentFlags().StringVar(&runOpts.rootMount, "root-mount", "/rootfs", "where the nodes root filesystem is mounted for chroot and file manipulation.")
+	runCmd.PersistentFlags().StringVar(&runOpts.healthCheckURL, "health-check-url", "", "HTTP(s) URL for the health check")
+}
+
+func runRunCmd(cmd *cobra.Command, args []string) error {
+	flag.Set("logtostderr", "true")
+	flag.Parse()
+
+	// To help debugging, immediately log version
+	glog.Infof("Version: %+v (%s)", version.Raw, version.Hash)
+
+	if runOpts.rootMount != "" {
+		glog.Infof(`Calling chroot("%s")`, runOpts.rootMount)
+		if err := syscall.Chroot(runOpts.rootMount); err != nil {
+			return fmt.Errorf("Unable to chroot to %s: %s", runOpts.rootMount, err)
+		}
+
+		glog.V(2).Infof("Moving to / inside the chroot")
+		if err := os.Chdir("/"); err != nil {
+			return fmt.Errorf("Unable to change directory to /: %s", err)
+		}
+	}
+
+	uri, err := url.Parse(runOpts.healthCheckURL)
+	if err != nil {
+		return fmt.Errorf("failed to parse health-check-url: %v", err)
+	}
+	if !uri.IsAbs() {
+		return fmt.Errorf("invalid URI %q (no scheme)", uri)
+	}
+
+	httpCheck, err := checkers.NewHTTP(&checkers.HTTPConfig{
+		URL: uri,
+		Client: &http.Client{Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create httpCheck: %v", err)
+	}
+
+	errCh := make(chan error)
+	tracker := &healthTracker{
+		ErrCh:            errCh,
+		SuccessThreshold: 2,
+		FailureThreshold: 10,
+		OnFailure:        func() error { return exec.Command("systemctl", "stop", runOpts.gcpRoutesService).Run() },
+		OnSuccess:        func() error { return exec.Command("systemctl", "start", runOpts.gcpRoutesService).Run() },
+	}
+
+	h := health.New()
+	h.AddChecks([]*health.Config{{
+		Name:       "dependency-check",
+		Checker:    httpCheck,
+		Interval:   time.Duration(5) * time.Second,
+		Fatal:      true,
+		OnComplete: tracker.OnComplete,
+	}})
+
+	if err := h.Start(); err != nil {
+		return fmt.Errorf("failed to start heath checker: %v", err)
+	}
+
+	for {
+		select {
+		case err := <-errCh:
+			if err != nil {
+				return fmt.Errorf("error running health checker: %v", err)
+			}
+		}
+	}
+}
+
+type trackerState int
+
+const (
+	unknownTrackerState trackerState = iota
+	failedTrackerState
+	succeededTrackerState
+)
+
+type healthTracker struct {
+	sync.Mutex
+
+	state trackerState
+
+	succeeded int
+	failed    int
+
+	// ErrCh is used to collect errors
+	ErrCh chan<- error
+
+	// SuccessThreshold is the number of consecutive success
+	SuccessThreshold int
+
+	// FailureThreshold is the number of consecutive failure that trigger OnFailure func
+	FailureThreshold int
+
+	// OnFailure is the function that is triggered when the health check is in FAILED state
+	// Non nil error are sent over the ErrCh
+	// Only one OnFailure function will be active at a time.
+	OnFailure func() error
+
+	// OnSuccess is the function that is triggered when the health check is in SUCCEEDED state
+	// Non nil error are sent over the ErrCh
+	// Only one OnFailure function will be active at a time.
+	OnSuccess func() error
+}
+
+func (sl *healthTracker) OnComplete(state *health.State) {
+	sl.Lock()
+	defer sl.Unlock()
+
+	switch state.Status {
+	case "ok":
+		sl.failed = 0
+		sl.succeeded++
+		if sl.succeeded >= sl.SuccessThreshold {
+			if sl.state != succeededTrackerState {
+				glog.Info("Running OnSuccess trigger")
+				if err := sl.OnSuccess(); err != nil {
+					sl.ErrCh <- err
+				}
+			}
+			sl.state = succeededTrackerState
+		}
+	case "failed":
+		sl.succeeded = 0
+		sl.failed++
+		if sl.failed >= sl.FailureThreshold {
+			if sl.state != failedTrackerState {
+				glog.Info("Running OnFailure trigger")
+				if err := sl.OnFailure(); err != nil {
+					sl.ErrCh <- err
+				}
+			}
+			sl.state = failedTrackerState
+		}
+	}
+}
