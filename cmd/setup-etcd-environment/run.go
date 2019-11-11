1994243255897cffbb11c1c73ae6de70bf67e7db
Sam Batschelet <sbatsche@redhat.com>
2019-11-11 16:11:10 -0500

    cmd/setup-etcd-environment: refactor and add tests
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- cmd/setup-etcd-environment/run.go
+++ cmd/setup-etcd-environment/run.go
@@ -25,8 +25,15 @@ import (
 )
 
 const (
-	EtcdScalingAnnotationKey = "etcd.operator.openshift.io/scale"
+	etcdBootstrapName        = "etcd-bootstrap"
 	etcdInitialExisting      = "existing"
+	EtcdScalingAnnotationKey = "etcd.operator.openshift.io/scale"
+	retryDuration            = 10 * time.Second
+	saTokenPath              = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+
+	// testing only
+	testHost      = "etcd-test.testcluster.openshift.com"
+	testIPAddress = "127.0.0.256"
 )
 
 var (
@@ -36,20 +43,61 @@ var (
 		Long:  "",
 		RunE:  runRunCmd,
 	}
-
-	runOpts struct {
-		discoverySRV string
-		ifName       string
-		outputFile   string
-		bootstrapSRV bool
-	}
+	runOpts opts
 )
+
+type opts struct {
+	discoverySRV string
+	ifName       string
+	outputFile   string
+	bootstrapSRV bool
+}
+
+type SetupEnv struct {
+	opts            *opts
+	etcdDNS         string
+	etcdName        string
+	etcdDataDir     string
+	etcdIP          string
+	Client          *kubernetes.Clientset
+	unsupportedArch string
+}
 
 func init() {
 	rootCmd.AddCommand(runCmd)
 	rootCmd.PersistentFlags().StringVar(&runOpts.discoverySRV, "discovery-srv", "", "DNS domain used to populate envs from SRV query.")
 	rootCmd.PersistentFlags().StringVar(&runOpts.outputFile, "output-file", "", "file where the envs are written. If empty, prints to Stdout.")
 	rootCmd.PersistentFlags().BoolVar(&runOpts.bootstrapSRV, "bootstrap-srv", true, "use SRV discovery for bootstraping etcd cluster.")
+}
+
+func newSetupEnv(runOpts *opts, etcdName, etcdDataDir string, ips []string) (*SetupEnv, error) {
+	dns, ip, err := validateMember(runOpts.discoverySRV, etcdName, ips)
+	if err != nil {
+		return nil, err
+	}
+
+	// enable etcd to run using s390 and s390x. Because these are not officially supported upstream
+	// etcd requires population of environment variable ETCD_UNSUPPORTED_ARCH at runtime.
+	// https://github.com/etcd-io/etcd/blob/master/Documentation/op-guide/supported-platform.md
+	var unsupportedArch string
+	arch := runtime.GOARCH
+	if strings.HasPrefix(arch, "s390") {
+		unsupportedArch = arch
+	}
+
+	s := &SetupEnv{
+		opts:            runOpts,
+		etcdName:        etcdName,
+		etcdDataDir:     etcdDataDir,
+		etcdDNS:         dns,
+		etcdIP:          ip,
+		unsupportedArch: unsupportedArch,
+	}
+	if err := s.setClient(); err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
 
 func runRunCmd(cmd *cobra.Command, args []string) error {
@@ -67,56 +115,31 @@ func runRunCmd(cmd *cobra.Command, args []string) error {
 	if etcdName == "" {
 		return fmt.Errorf("environment variable ETCD_NAME has no value")
 	}
-
 	etcdDataDir := os.Getenv("ETCD_DATA_DIR")
 	if etcdDataDir == "" {
 		return fmt.Errorf("environment variable ETCD_DATA_DIR has no value")
 	}
-
 	if !inCluster() {
 		glog.V(4).Infof("KUBERNETES_SERVICE_HOST or KUBERNETES_SERVICE_PORT contain no value, running in standalone mode.")
 	}
 
-	ips, err := ipAddrs()
+	// preferredIP allows for a specific IP to be used as long as it is found on the interface. If found
+	// use it to populate `ETCD_IPV4_ADDRESS` on disk otherwise we fallback to default lookup list.
+	preferredIP := os.Getenv("ETCD_IPV4_ADDRESS")
+
+	ips, err := ipAddrs(preferredIP)
 	if err != nil {
 		return err
 	}
 
-	var dns string
-	var ip string
-	if err := wait.PollImmediate(30*time.Second, 5*time.Minute, func() (bool, error) {
-		for _, cand := range ips {
-			found, err := reverseLookup("etcd-server-ssl", "tcp", runOpts.discoverySRV, cand, runOpts.bootstrapSRV)
-			if err != nil {
-				glog.Errorf("error looking up self for candidate IP %s: %v", cand, err)
-				continue
-			}
-			if found != "" {
-				dns = found
-				ip = cand
-				return true, nil
-			}
-			glog.V(4).Infof("no matching dns for %s in %s: %v", cand, runOpts.discoverySRV, err)
-		}
-		return false, nil
-	}); err != nil {
-		return fmt.Errorf("could not find self: %v", err)
+	setupEnv, err := newSetupEnv(&runOpts, etcdName, etcdDataDir, ips)
+	if err != nil {
+		return err
 	}
-	glog.Infof("dns name is %s", dns)
 
-	var exportEnv map[string]string
-
-	if _, err := os.Stat(fmt.Sprintf("%s/member", etcdDataDir)); os.IsNotExist(err) && !runOpts.bootstrapSRV && inCluster() {
-		exportEnv, err = setExportEnv(etcdName, dns)
-		if err != nil {
-			return err
-		}
-	} else {
-		// initialize envs used to bootstrap etcd
-		exportEnv, err = setBootstrapEnv(runOpts.outputFile, runOpts.discoverySRV, runOpts.bootstrapSRV)
-		if err != nil {
-			return err
-		}
+	exportEnv, err := setupEnv.getExportEnv()
+	if err != nil {
+		return err
 	}
 
 	out := os.Stdout
@@ -129,85 +152,52 @@ func runRunCmd(cmd *cobra.Command, args []string) error {
 		out = f
 	}
 
-	if runOpts.bootstrapSRV {
-		exportEnv["DISCOVERY_SRV"] = runOpts.discoverySRV
-	} else {
-		exportEnv["NAME"] = etcdName
-	}
-
-	// enable etcd to run using s390 and s390x. Because these are not officially supported upstream
-	// etcd requires population of environment variable ETCD_UNSUPPORTED_ARCH at runtime.
-	// https://github.com/etcd-io/etcd/blob/master/Documentation/op-guide/supported-platform.md
-	arch := runtime.GOARCH
-	if strings.HasPrefix(arch, "s390") {
-		exportEnv["UNSUPPORTED_ARCH"] = arch
-	}
 	if err := writeEnvironmentFile(exportEnv, out, true); err != nil {
 		return err
 	}
 
-	return writeEnvironmentFile(map[string]string{
-		"IPV4_ADDRESS":      ip,
-		"DNS_NAME":          dns,
-		"WILDCARD_DNS_NAME": fmt.Sprintf("*.%s", runOpts.discoverySRV),
-	}, out, false)
+	unexportedEnv := map[string]string{
+		"IPV4_ADDRESS":      setupEnv.etcdIP,
+		"WILDCARD_DNS_NAME": fmt.Sprintf("*.%s", setupEnv.opts.discoverySRV),
+	}
+	if setupEnv.etcdDNS != "" {
+		unexportedEnv["DNS_NAME"] = setupEnv.etcdDNS
+	}
+	return writeEnvironmentFile(unexportedEnv, out, false)
 }
 
-func setExportEnv(etcdName, dns string) (map[string]string, error) {
-	exportEnv := make(map[string]string)
-	duration := 10 * time.Second
-	wait.PollInfinite(duration, func() (bool, error) {
-		if _, err := os.Stat("/var/run/secrets/kubernetes.io/serviceaccount/token"); os.IsNotExist(err) {
-			glog.Errorf("serviceaccount failed: %v", err)
-			return false, nil
-		}
-		return true, nil
-	})
-
-	clientConfig, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
+func (s *SetupEnv) getExportEnv() (map[string]string, error) {
+	// if etcd is managed by the operator populate ENV from configmap
+	if s.isEtcdScaling() {
+		return s.getOperatorEnv()
 	}
-	client, err := kubernetes.NewForConfig(clientConfig)
-	if err != nil {
-		return nil, fmt.Errorf("error creating client: %v", err)
-	}
-	var e ceoapi.EtcdScaling
-	// wait forever for success and retry every duration interval
-	wait.PollInfinite(duration, func() (bool, error) {
-		result, err := client.CoreV1().ConfigMaps("openshift-etcd").Get("member-config", metav1.GetOptions{})
-		if err != nil {
-			glog.Errorf("error creating client %v", err)
-			return false, nil
-		}
-		if err := json.Unmarshal([]byte(result.Annotations[EtcdScalingAnnotationKey]), &e); err != nil {
-			glog.Errorf("error decoding result %v", err)
-			return false, nil
-		}
-		if e.Metadata.Name != etcdName {
-			glog.Errorf("could not find self in member-config")
-			return false, nil
-		}
-		members := e.Members
-		if len(members) == 0 {
-			glog.Errorf("no members found in member-config")
-			return false, nil
-		}
-		var memberList []string
-		for _, m := range members {
-			memberList = append(memberList, fmt.Sprintf("%s=%s", m.Name, m.PeerURLS[0]))
-		}
-		memberList = append(memberList, fmt.Sprintf("%s=https://%s:2380", etcdName, dns))
-		exportEnv["INITIAL_CLUSTER"] = strings.Join(memberList, ",")
-		exportEnv["INITIAL_CLUSTER_STATE"] = etcdInitialExisting
-		return true, nil
-	})
+	// initialize envs used to bootstrap etcd using SRV discovery or disaster recovery.
+	return s.getBootstrapEnv()
+}
 
-	ep, err := client.CoreV1().Endpoints("openshift-etcd").Get("etcd", metav1.GetOptions{})
+// getOperatorEnv returns a maping of ENV variables required to scale etcd via operator
+func (s *SetupEnv) getOperatorEnv() (map[string]string, error) {
+	env := make(map[string]string)
+	env["NAME"] = s.etcdName
+
+	memberList := s.getEtcdMemberList()
+	env["INITIAL_CLUSTER"] = strings.Join(memberList, ",")
+	env["INITIAL_CLUSTER_STATE"] = etcdInitialExisting
+
+	endpoints, err := s.getEtcdEndpoints()
 	if err != nil {
 		return nil, err
 	}
-	hostEtcdEndpoint, err := client.CoreV1().Endpoints("openshift-etcd").Get("host-etcd", metav1.GetOptions{})
+	env["ENDPOINTS"] = strings.Join(endpoints, ",")
+	return env, nil
+}
+
+func (s *SetupEnv) getEtcdEndpoints() ([]string, error) {
+	ep, err := s.Client.CoreV1().Endpoints("openshift-etcd").Get("etcd", metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	hostEtcdEndpoint, err := s.Client.CoreV1().Endpoints("openshift-etcd").Get("host-etcd", metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
@@ -217,9 +207,13 @@ func setExportEnv(etcdName, dns string) (map[string]string, error) {
 
 	endpoints := make([]string, 0)
 	for _, member := range hostEtcdEndpoint.Subsets[0].Addresses {
-		if member.Hostname == "etcd-bootstrap" {
-			endpoints = append(endpoints, "https://etcd-bootstrap."+runOpts.discoverySRV+":2379")
-			break
+		// TODO validate IP?
+		if member.Hostname == etcdBootstrapName {
+			if member.IP != "" {
+				endpoints = append(endpoints, fmt.Sprintf("https://%s:2379", member.IP))
+				break
+			}
+			return nil, fmt.Errorf("openshift-etcd/host-etcd hostname %s has no IP", etcdBootstrapName)
 		}
 	}
 	if len(ep.Subsets) != 1 {
@@ -228,35 +222,106 @@ func setExportEnv(etcdName, dns string) (map[string]string, error) {
 	for _, s := range ep.Subsets[0].Addresses {
 		endpoints = append(endpoints, "https://"+s.IP+":2379")
 	}
-
-	exportEnv["ENDPOINTS"] = strings.Join(endpoints, ",")
-	return exportEnv, nil
+	return endpoints, nil
 }
 
-// setBootstrapEnv populates and returns a map based on envs from file.
-func setBootstrapEnv(envFile, discoverySRV string, bootstrapSRV bool) (map[string]string, error) {
+func (s *SetupEnv) getEtcdMemberList() []string {
+	var memberList []string
+
+	// wait forever for success and retry every duration interval
+	wait.PollInfinite(retryDuration, func() (bool, error) {
+		var e ceoapi.EtcdScaling
+		result, err := s.Client.CoreV1().ConfigMaps("openshift-etcd").Get("member-config", metav1.GetOptions{})
+		if err != nil {
+			glog.Errorf("error creating client %v", err)
+			return false, nil
+		}
+		if err := json.Unmarshal([]byte(result.Annotations[EtcdScalingAnnotationKey]), &e); err != nil {
+			glog.Errorf("error decoding result %v", err)
+			return false, nil
+		}
+		if e.Metadata.Name != s.etcdName {
+			glog.Errorf("could not find self in member-config")
+			return false, nil
+		}
+		members := e.Members
+		if len(members) == 0 {
+			glog.Errorf("no members found in member-config")
+			return false, nil
+		}
+		for _, m := range members {
+			memberList = append(memberList, fmt.Sprintf("%s=%s", m.Name, m.PeerURLS[0]))
+		}
+		memberList = append(memberList, fmt.Sprintf("%s=https://%s:2380", s.etcdName, s.etcdDNS))
+		return true, nil
+	})
+	return memberList
+}
+
+// getBootstrapEnv populates ENV variables used for bootstrapping etcd with SRV and recovery. We take care
+// to persist important ENV during reboot. We also make sure to not mix ENV that might cause etcd to fail to start.
+func (s *SetupEnv) getBootstrapEnv() (map[string]string, error) {
 	bootstrapEnv := make(map[string]string)
-	if _, err := os.Stat(envFile); !os.IsNotExist(err) {
-		recoveryEnv, err := godotenv.Read(envFile)
+	if _, err := os.Stat(s.opts.outputFile); !os.IsNotExist(err) {
+		// read existing ENV file
+		existingEnv, err := godotenv.Read(s.opts.outputFile)
 		if err != nil {
 			return nil, err
 		}
-		// persist any observed envs used for recovery
+		// persist any observed envs used for recovery. We want to make sure that ETCD_INITIAL_CLUSTER and
+		// ETCD_INITIAL_CLUSTER_STATE survive reboot.
 		for _, val := range []string{"INITIAL_CLUSTER", "INITIAL_CLUSTER_STATE"} {
 			rkey := fmt.Sprintf("ETCD_%s", val)
-			if recoveryEnv[rkey] != "" {
-				bootstrapEnv[val] = recoveryEnv[rkey]
+			if existingEnv[rkey] != "" {
+				bootstrapEnv[val] = existingEnv[rkey]
 			}
 		}
 	}
-	// define srv discovery if enabeled and not in recovery mode
-	if len(bootstrapEnv) == 0 && bootstrapSRV {
-		bootstrapEnv["DISCOVERY_SRV"] = discoverySRV
+
+	// Define SRV discovery variables if enabeled and not in recovery mode. This is important because we can
+	// not mix `ETCD_DISCOVERY_SRV` with ETCD_INITIAL_CLUSTER` or etcd will fail to start.
+	if len(bootstrapEnv) == 0 && s.opts.bootstrapSRV {
+		bootstrapEnv["DISCOVERY_SRV"] = s.opts.discoverySRV
 	}
 	return bootstrapEnv, nil
 }
 
-func ipAddrs() ([]string, error) {
+// isEtcdScaling performs a series of checks to see if etcd is currently attempting to scale from the
+// `cluster-etcd-operator` and returns true if yes. If false we can conclude that the member is already
+// part of the cluster or we have/are bootstrapping etcd using SRV discovery.
+func (s *SetupEnv) isEtcdScaling() bool {
+	if _, err := os.Stat(fmt.Sprintf("%s/member", s.etcdDataDir)); os.IsNotExist(err) && !s.opts.bootstrapSRV && inCluster() {
+		return true
+	}
+	return false
+}
+
+func (s *SetupEnv) setClient() error {
+	if inCluster() && !s.opts.bootstrapSRV {
+		wait.PollInfinite(retryDuration, func() (bool, error) {
+			if _, err := os.Stat(saTokenPath); os.IsNotExist(err) {
+				glog.Errorf("serviceaccount failed: %v", err)
+				return false, nil
+			}
+			return true, nil
+		})
+		clientConfig, err := rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+		client, err := kubernetes.NewForConfig(clientConfig)
+		if err != nil {
+			return fmt.Errorf("error creating client: %v", err)
+		}
+		s.Client = client
+	}
+	glog.Infof("no client set")
+	return nil
+}
+
+// ipAddrs checks the local interfaces and returns a populated list of addresses as a slice of string.
+// This function also accepts an optional preferredIP address which if found will be explicitly used.
+func ipAddrs(preferredIP string) ([]string, error) {
 	var ips []string
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
@@ -283,17 +348,20 @@ func ipAddrs() ([]string, error) {
 		ips = append(ips, ip.String())
 	}
 
+	// validate preferredIP is valid for this system or fall back to default list.
+	if preferredIP != "" {
+		for _, ip := range ips {
+			if ip == preferredIP {
+				glog.Infof("ipAddrs: preferredIP %s found", preferredIP)
+				return []string{preferredIP}, nil
+			}
+		}
+		glog.Infof("ipAddrs: preferredIP %s not found on system interfaces", preferredIP)
+	}
 	return ips, nil
 }
 
-func reverseLookup(service, proto, name, self string, bootstrapSRV bool) (string, error) {
-	if bootstrapSRV || inCluster() {
-		return reverseLookupSelf(service, proto, name, self)
-	}
-	return lookupTargetMatchSelf(fmt.Sprintf("etcd-bootstrap.%s", name), self)
-}
-
-// returns the target from the SRV record that resolves to self.
+// reverseLookupSelf returns the target from the SRV record that resolves to self.
 func reverseLookupSelf(service, proto, name, self string) (string, error) {
 	_, srvs, err := net.LookupSRV(service, proto, name)
 	if err != nil {
@@ -312,13 +380,14 @@ func reverseLookupSelf(service, proto, name, self string) (string, error) {
 	return "", fmt.Errorf("could not find self")
 }
 
-//
+// lookupTargetMatchSelf takes a target FQDN and performs a DNS lookup. If the result of the
+// lookup addresses matches self we return that address.
 func lookupTargetMatchSelf(target, self string) (string, error) {
+	var selfTarget string
 	addrs, err := net.LookupHost(target)
 	if err != nil {
-		return "", fmt.Errorf("could not resolve member %q", target)
+		return selfTarget, fmt.Errorf("could not resolve member %q", target)
 	}
-	selfTarget := ""
 	for _, addr := range addrs {
 		if addr == self {
 			selfTarget = strings.Trim(target, ".")
@@ -348,4 +417,41 @@ func inCluster() bool {
 		return false
 	}
 	return true
+}
+
+// validateMember performs an SRV lookup for a service based on the cluster domain. If an IP address
+// found in the list of IPs for the system we return that IP address and the matching FQDN.
+func validateMember(discoverySRV, etcdName string, ips []string) (string, string, error) {
+	var dns string
+	var ip string
+	// for testing only
+	if len(ips) > 0 && ips[0] == testIPAddress {
+		return testHost, testIPAddress, nil
+	}
+
+	// bootstrap is a special case we do not use FQDN
+	if etcdName == etcdBootstrapName {
+		return dns, ips[0], nil
+	}
+
+	if err := wait.PollImmediate(30*time.Second, 5*time.Minute, func() (bool, error) {
+		for _, cand := range ips {
+			found, err := reverseLookupSelf("etcd-server-ssl", "tcp", discoverySRV, cand)
+			if err != nil {
+				glog.Errorf("error looking up self for candidate IP %s: %v", cand, err)
+				continue
+			}
+			if found != "" {
+				dns = found
+				ip = cand
+				return true, nil
+			}
+			glog.V(4).Infof("no matching dns for %s in %s: %v", cand, discoverySRV, err)
+		}
+		return false, nil
+	}); err != nil {
+		return dns, ip, fmt.Errorf("could not find self: %v", err)
+	}
+	glog.Infof("dns name is %s", dns)
+	return dns, ip, nil
 }
