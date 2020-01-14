347971da2135d00869fd20388ed3bc9364a43b91
sgreene570 <sgreene570@gmail.com>
2020-01-14 14:00:42 -0500

    Fix update codegen script to be compatible with go mod vendor

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
-${SCRIPT_ROOT}/vendor/k8s.io/code-generator/generate-groups.sh all \
+bash ${SCRIPT_ROOT}/vendor/k8s.io/code-generator/generate-groups.sh all \
   github.com/openshift/machine-config-operator/pkg/generated github.com/openshift/machine-config-operator/pkg/apis \
   "machineconfiguration.openshift.io:v1" \
   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
