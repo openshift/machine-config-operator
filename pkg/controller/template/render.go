f84b9c9d546b4f40c40ecf5b226e4a6ee70b83b7
Sinny Kumari <sinny@redhat.com>
2020-04-16 20:16:24 +0530

    cloudConfig: read cloud Config from openshift-config-managed/kube-cloud-config ConfigMap
    cloudConfig is now generated by kube_cloud_config controller for all supported
    platforms. Controller generates kube-cloud-config ConfigMap in openshift-config-managed
    namespace where cloud.conf key is stored.
    
    Links:
    - https://github.com/openshift/enhancements/blob/master/enhancements/installer/aws-custom-region-and-endpoints.md
    - https://github.com/openshift/api/pull/599
    - https://github.com/openshift/api/pull/621

--- pkg/controller/template/render.go
+++ pkg/controller/template/render.go
@@ -504,7 +504,7 @@ func cloudConfigFlag(cfg RenderConfig) interface{} {
 	}
 	flag := "--cloud-config=/etc/kubernetes/cloud.conf"
 	switch cfg.Platform {
-	case platformAzure, platformOpenStack:
+	case platformAWS, platformAzure, platformBaremetal, platformGCP, platformOpenStack, platformOvirt, platformVSphere:
 		return flag
 	default:
 		return ""
