8bc8c35c7a7e499f4b6909e6d38b959bad80d90d
Antonio Murdaca <runcom@linux.com>
2020-05-28 10:11:53 +0200

    MCD: tolerate all taints
    Signed-off-by: Antonio Murdaca <runcom@linux.com>

--- pkg/operator/assets/bindata.go
+++ pkg/operator/assets/bindata.go
@@ -1402,13 +1402,6 @@ spec:
       hostPID: true
       serviceAccountName: machine-config-daemon
       terminationGracePeriodSeconds: 600
-      tolerations:
-      - key: node-role.kubernetes.io/master
-        operator: Exists
-        effect: NoSchedule
-      - key: node-role.kubernetes.io/etcd
-        operator: Exists
-        effect: NoSchedule
       nodeSelector:
         kubernetes.io/os: linux
       priorityClassName: "system-node-critical"
@@ -1422,7 +1415,9 @@ spec:
         - name: cookie-secret
           secret:
             secretName: cookie-secret
-`)
+      tolerations:
+      # MCD needs to run everywhere. Tolerate all taints.
+      - operator: Exists`)
 
 func manifestsMachineconfigdaemonDaemonsetYamlBytes() ([]byte, error) {
 	return _manifestsMachineconfigdaemonDaemonsetYaml, nil
