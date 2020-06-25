ee2aa585ad5b188c783b337840852e4b477e114e
Antonio Murdaca <runcom@users.noreply.github.com>
2020-06-25 18:06:55 +0200

    Update pkg/daemon/pivot/types/imageinspection.go
    Co-authored-by: Stephen Milner <stevem@gnulinux.net>

--- pkg/daemon/pivot/types/imageinspection.go
+++ pkg/daemon/pivot/types/imageinspection.go
@@ -5,7 +5,7 @@ const (
 	PivotNamePrefix = "ostree-container-pivot-"
 
 	// PivotFailurePath is the path pivot writes its error to when it fails. We need this to bubble
-	// up any error happning there as we just log those errors in MCD and are useful unless we report
-	// them up the stack
+	// up any errors since we only log those errors in MCD. If we don't do this then the error messages
+	// don't get visibility up the stack.
 	PivotFailurePath = "/run/pivot/failure"
 )
