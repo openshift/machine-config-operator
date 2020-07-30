c4ffe75a80b64d0783d4d174aa1bcc50dcfdbed5
Sinny Kumari <sinny@redhat.com>
2020-07-30 11:19:06 +0530

    daemon: perform tuning kargs processing during update()
    update() function runs during firstboot as well as subsequent
    boot.

--- pkg/daemon/update.go
+++ pkg/daemon/update.go
@@ -406,6 +406,16 @@ func (dn *Daemon) update(oldConfig, newConfig *mcfgv1.MachineConfig) (retErr err
 		}
 	}()
 
+	// Ideally we would want to update kernelArguments only via MachineConfigs.
+	// We are keeping this to maintain compatibility and OKD requirement.
+	tuningChanged, err := UpdateTuningArgs(KernelTuningFile, CmdLineFile)
+	if err != nil {
+		return err
+	}
+	if tuningChanged {
+		glog.Info("Updated kernel tuning arguments")
+	}
+
 	return dn.finalizeAndReboot(newConfig)
 }
 
