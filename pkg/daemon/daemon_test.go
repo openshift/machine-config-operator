5c49ad77958bfda670f11b111e62340d7344ad3c
Colin Walters <walters@verbum.org>
2020-07-31 15:30:06 +0000

    daemon: Remove "compare digest" code for OS updates
    We don't need to do this special casing anymore.  This
    corner case only arises for the synthetic `e2e-gcp-upgrade`
    test, and we actually want to test the OS update path there.
    
    Also remove the `dummy` URL since that's no longer used.  We
    could probably even remove the empty case but I'm less certain
    that doesn't happen with the once-from path on non-CoreOS systems.
    
    Closes: https://github.com/openshift/machine-config-operator/issues/1964

--- pkg/daemon/daemon_test.go
+++ pkg/daemon/daemon_test.go
@@ -114,25 +114,20 @@ func TestCompareOSImageURL(t *testing.T) {
 	refA := "registry.example.com/foo/bar@sha256:0743a3cc3bcf3b4aabb814500c2739f84cb085ff4e7ec7996aef7977c4c19c7f"
 	refB := "registry.example.com/foo/baz@sha256:0743a3cc3bcf3b4aabb814500c2739f84cb085ff4e7ec7996aef7977c4c19c7f"
 	refC := "registry.example.com/foo/bar@sha256:2a76681fd15bfc06fa4aa0ff6913ba17527e075417fc92ea29f6bcc2afca24ff"
-	m, err := compareOSImageURL(refA, refA)
+	m := compareOSImageURL(refA, refA)
 	if !m {
 		t.Fatalf("Expected refA ident")
 	}
-	m, err = compareOSImageURL(refA, refB)
-	if !m {
-		t.Fatalf("Expected refA = refB")
+	m = compareOSImageURL(refA, refB)
+	if m {
+		t.Fatalf("Expected refA != refB")
 	}
-	m, err = compareOSImageURL(refA, refC)
+	m = compareOSImageURL(refA, refC)
 	if m {
 		t.Fatalf("Expected refA != refC")
 	}
-	m, err = compareOSImageURL(refA, "registry.example.com/foo/bar")
-	if m || err == nil {
-		t.Fatalf("Expected err")
-	}
-	m, err = compareOSImageURL("", refA)
+	m = compareOSImageURL("", refA)
 	assert.False(t, m)
-	assert.Nil(t, err)
 }
 
 type fixture struct {
