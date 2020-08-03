b865324d1620c6903dd718c203f428f1cde67ad5
Colin Walters <walters@verbum.org>
2020-08-03 15:09:00 +0000

    tests: Fix capitalization
    My IDE warns about this - it's the only warning.

--- pkg/daemon/update_test.go
+++ pkg/daemon/update_test.go
@@ -152,7 +152,7 @@ func TestReconcilable(t *testing.T) {
 	checkIrreconcilableResults(t, "PasswdGroups", isReconcilable)
 }
 
-func TestmachineConfigDiff(t *testing.T) {
+func TestMachineConfigDiff(t *testing.T) {
 	oldIgnCfg := ctrlcommon.NewIgnConfig()
 	oldConfig := helpers.CreateMachineConfigFromIgnition(oldIgnCfg)
 	oldConfig.ObjectMeta = metav1.ObjectMeta{Name: "oldconfig"}
