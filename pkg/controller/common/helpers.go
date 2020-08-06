acad01fd1e8d5eda282e197c9cd616cf567e8793
Urvashi Mohnani <umohnani@redhat.com>
2020-08-06 10:22:01 -0400

    Check the version hash so we resync on an upgrade
    Add a check for the version hash to ensure that we resync
    and generate a new controller version during an upgrade.
    
    Update the way we append the status for a ctrcfg as well -
    ensure that we don't have mutliple of the same status recorded.
    
    Add a check to avoid adding the same mc name more than once in
    the list of finalizers. When we update an existing ctrcfg, the
    generation number changes causing a resync and we don't need to add
    the same name to the list of finalizers every time this happens.
    
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

--- pkg/controller/common/helpers.go
+++ pkg/controller/common/helpers.go
@@ -337,3 +337,13 @@ func InSlice(elem string, slice []string) bool {
 	}
 	return false
 }
+
+// InSlice search for an element in slice and return true if found, otherwise return false
+func InSlice(elem string, slice []string) bool {
+	for _, k := range slice {
+		if k == elem {
+			return true
+		}
+	}
+	return false
+}
