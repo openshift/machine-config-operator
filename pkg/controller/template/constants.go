6da078f04ecad4c8446e4539dc7f1a8cda434722
Sam Batschelet <sbatsche@redhat.com>
2020-08-19 18:00:02 -0400

    cleanup: remove legacy etcd bits
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- pkg/controller/template/constants.go
+++ pkg/controller/template/constants.go
@@ -1,23 +1,11 @@
 package template
 
 const (
-	// EtcdImageKey is the key that references the etcd image in the controller
-	EtcdImageKey string = "etcdKey"
-
-	// SetupEtcdEnvKey is the key that references the setup-etcd-environment image in the controller
-	SetupEtcdEnvKey string = "setupEtcdEnvKey"
-
 	// GCPRoutesControllerKey is the key that references the gcp-routes-controller image in the controller
 	GCPRoutesControllerKey string = "gcpRoutesControllerKey"
 
 	// InfraImageKey is the key that references the infra image in the controller for crio.conf
 	InfraImageKey string = "infraImageKey"
-
-	// KubeClientAgentImageKey is the key that references the kube-client-agent image in the controller
-	KubeClientAgentImageKey string = "kubeClientAgentImageKey"
-
-	// ClusterEtcdOperatorImageKey is the key that references the cluster-etcd-operator image in the controller
-	ClusterEtcdOperatorImageKey string = "clusterEtcdOperatorImageKey"
 
 	// KeepalivedKey is the key that references the keepalived-ipfailover image in the controller
 	KeepalivedKey string = "keepalivedImage"
