b664f121c560a9cdc508296b724576bde21b5fea
Kirsten Garrison <kikis.github@gmail.com>
2021-04-08 12:32:09 -0700

    sync.go: add syncUpgradeableStatus() call when master pool not leveled
    Upgradeable = false when required pool not leveled, even though in this instance
    the upgrade is blocked from completing. Adds clarity for user experience.

--- pkg/operator/sync.go
+++ pkg/operator/sync.go
@@ -611,6 +611,10 @@ func (optr *Operator) syncRequiredMachineConfigPools(_ *renderConfig) error {
 				if err := isMachineConfigPoolConfigurationValid(pool, version.Hash, optr.mcLister.Get); err != nil {
 					lastErr = fmt.Errorf("pool %s has not progressed to latest configuration: %v, retrying", pool.Name, err)
 					glog.Info(lastErr.Error())
+					syncerr := optr.syncUpgradeableStatus()
+					if syncerr != nil {
+						glog.Errorf("Error syncingUpgradeableStatus: %q", syncerr)
+					}
 					return false, nil
 				}
 
@@ -621,6 +625,10 @@ func (optr *Operator) syncRequiredMachineConfigPools(_ *renderConfig) error {
 				}
 				lastErr = fmt.Errorf("error required pool %s is not ready, retrying. Status: (total: %d, ready %d, updated: %d, unavailable: %d, degraded: %d)", pool.Name, pool.Status.MachineCount, pool.Status.ReadyMachineCount, pool.Status.UpdatedMachineCount, pool.Status.UnavailableMachineCount, pool.Status.DegradedMachineCount)
 				glog.Info(lastErr.Error())
+				syncerr := optr.syncUpgradeableStatus()
+				if syncerr != nil {
+					glog.Errorf("Error syncingUpgradeableStatus: %q", syncerr)
+				}
 				return false, nil
 			}
 		}
