057abe55b65ee31df36f775e50f587fccb52875e
Stephen Lowrie <slowrie@redhat.com>
2021-06-11 12:08:56 -0500

    pkg/server: add GetKernelArguments to Server interface
    Split the fetching of the appended MachineConfig / Ignition config into
    a separate getMachineConfig function for both the bootstrap & cluster
    server and add a new GetKernelArguments function.
    
    Prep work for performing a modification of the resultant Ignition config
    on specific spec versions based on fields from the MachineConfig.

--- pkg/server/bootstrap_server.go
+++ pkg/server/bootstrap_server.go
@@ -11,6 +11,7 @@ import (
 	"github.com/golang/glog"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientcmd "k8s.io/client-go/tools/clientcmd/api/v1"
+	igntypes "github.com/coreos/ignition/v2/config/v3_2/types"
 
 	mcfgv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
 	ctrlcommon "github.com/openshift/machine-config-operator/pkg/controller/common"
@@ -41,7 +42,7 @@ func NewBootstrapServer(dir, kubeconfig string) (Server, error) {
 	}, nil
 }
 
-// GetConfig fetches the machine config(type - Ignition) from the bootstrap server,
+// getMachineConfig fetches the machine config from the bootstrap server,
 // based on the pool request.
 // If a config cannot be found or parsed, it returns a nil conf, along with an error.
 //
@@ -57,9 +58,9 @@ func NewBootstrapServer(dir, kubeconfig string) (Server, error) {
 // 3. Load the machine config.
 // 4. Append the machine annotations file.
 // 5. Append the KubeConfig file.
-func (bsc *bootstrapServer) GetConfig(cr poolRequest) (*runtime.RawExtension, error) {
+func (bsc *bootstrapServer) getMachineConfig(cr poolRequest) (*mcfgv1.MachineConfig, *igntypes.Config, error) {
 	if cr.machineConfigPool != "master" {
-		return nil, fmt.Errorf("refusing to serve bootstrap configuration to pool %q", cr.machineConfigPool)
+		return nil, nil, fmt.Errorf("refusing to serve bootstrap configuration to pool %q", cr.machineConfigPool)
 	}
 	// 1. Read the Machine Config Pool object.
 	fileName := path.Join(bsc.serverBaseDir, "machine-pools", cr.machineConfigPool+".yaml")
@@ -67,16 +68,16 @@ func (bsc *bootstrapServer) GetConfig(cr poolRequest) (*runtime.RawExtension, er
 	data, err := ioutil.ReadFile(fileName)
 	if os.IsNotExist(err) {
 		glog.Errorf("could not find file: %s", fileName)
-		return nil, nil
+		return nil, nil, nil
 	}
 	if err != nil {
-		return nil, fmt.Errorf("server: could not read file %s, err: %v", fileName, err)
+		return nil, nil, fmt.Errorf("server: could not read file %s, err: %v", fileName, err)
 	}
 
 	mp := new(mcfgv1.MachineConfigPool)
 	err = yaml.Unmarshal(data, mp)
 	if err != nil {
-		return nil, fmt.Errorf("server: could not unmarshal file %s, err: %v", fileName, err)
+		return nil, nil, fmt.Errorf("server: could not unmarshal file %s, err: %v", fileName, err)
 	}
 
 	currConf := mp.Status.Configuration.Name
@@ -87,27 +88,42 @@ func (bsc *bootstrapServer) GetConfig(cr poolRequest) (*runtime.RawExtension, er
 	data, err = ioutil.ReadFile(fileName)
 	if os.IsNotExist(err) {
 		glog.Errorf("could not find file: %s", fileName)
-		return nil, nil
+		return nil, nil, nil
 	}
 	if err != nil {
-		return nil, fmt.Errorf("server: could not read file %s, err: %v", fileName, err)
+		return nil, nil, fmt.Errorf("server: could not read file %s, err: %v", fileName, err)
 	}
 
 	mc := new(mcfgv1.MachineConfig)
 	err = yaml.Unmarshal(data, mc)
 	if err != nil {
-		return nil, fmt.Errorf("server: could not unmarshal file %s, err: %v", fileName, err)
+		return nil, nil, fmt.Errorf("server: could not unmarshal file %s, err: %v", fileName, err)
 	}
 	ignConf, err := ctrlcommon.ParseAndConvertConfig(mc.Spec.Config.Raw)
 	if err != nil {
-		return nil, fmt.Errorf("parsing Ignition config failed with error: %v", err)
+		return nil, nil, fmt.Errorf("parsing Ignition config failed with error: %v", err)
 	}
 
 	appenders := getAppenders(currConf, nil, bsc.kubeconfigFunc)
 	for _, a := range appenders {
 		if err := a(&ignConf, mc); err != nil {
-			return nil, err
+			return nil, nil, err
 		}
+	}
+
+	return mc, &ignConf, nil
+}
+
+// GetConfig fetches the machine conf(type - Ignition) based on the pool request
+func (bsc *bootstrapServer) GetConfig(cr poolRequest) (*runtime.RawExtension, error) {
+	_, ignConf, err := bsc.getMachineConfig(cr)
+	if err != nil {
+		return nil, err
+	}
+	// exit without error if the machine config file was not readable and resulted in no
+	// ignition config being fetched
+	if ignConf == nil {
+		return nil, nil
 	}
 
 	rawConf, err := json.Marshal(ignConf)
@@ -115,6 +131,21 @@ func (bsc *bootstrapServer) GetConfig(cr poolRequest) (*runtime.RawExtension, er
 		return nil, err
 	}
 	return &runtime.RawExtension{Raw: rawConf}, nil
+}
+
+// GetKernelArguments fetches the machine config kernel arguments based on the pool request
+func (bsc *bootstrapServer) GetKernelArguments(cr poolRequest) ([]string, error) {
+	mc, _, err := bsc.getMachineConfig(cr)
+	if err != nil {
+		return nil, err
+	}
+	// exit without error if the machine config file was not readable and resulted in
+	// no machine config being fetched
+	if mc == nil {
+		return nil, nil
+	}
+
+	return mc.Spec.KernelArguments, nil
 }
 
 func kubeconfigFromFile(path string) ([]byte, []byte, error) {
