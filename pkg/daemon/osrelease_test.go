4609a19626d0fb17a425add8aad0c8f507909382
John Kyros <79665180+jkyros@users.noreply.github.com>
2021-07-01 14:20:43 -0500

    osrelease_test: add test for RHEL8 OS detection
    Added test to make sure RHEL8 is properly detected, much like RHEL7

--- pkg/daemon/osrelease_test.go
+++ pkg/daemon/osrelease_test.go
@@ -16,3 +16,15 @@ func TestIsLikeTraditionalRHEL7(t *testing.T) {
 	testOS.VersionID = "6.8"
 	assert.False(t, testOS.IsLikeTraditionalRHEL7())
 }
+
+func TestIsLikeTraditionalRHEL8(t *testing.T) {
+	var testOS OperatingSystem
+	testOS.VersionID = "8"
+	assert.True(t, testOS.IsLikeTraditionalRHEL8())
+	testOS.VersionID = "8.5"
+	assert.True(t, testOS.IsLikeTraditionalRHEL8())
+	testOS.VersionID = "7"
+	assert.False(t, testOS.IsLikeTraditionalRHEL8())
+	testOS.VersionID = "6.8"
+	assert.False(t, testOS.IsLikeTraditionalRHEL8())
+}
