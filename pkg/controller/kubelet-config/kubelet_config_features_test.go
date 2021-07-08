36b4168b0028c2617c0b7853e51e8fb5f202914a
Joel Speed <joel.speed@hotmail.co.uk>
2021-07-08 12:47:45 +0100

    Add tests for KubeletConfig Feature bootstrap

--- pkg/controller/kubelet-config/kubelet_config_features_test.go
+++ pkg/controller/kubelet-config/kubelet_config_features_test.go
@@ -11,8 +11,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubeletconfigv1beta1 "k8s.io/kubelet/config/v1beta1"
 
+	mcfgv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
 	ctrlcommon "github.com/openshift/machine-config-operator/pkg/controller/common"
 	"github.com/openshift/machine-config-operator/test/helpers"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFeatureGateDrift(t *testing.T) {
@@ -128,6 +130,81 @@ func TestFeaturesCustomNoUpgrade(t *testing.T) {
 			f.expectGetMachineConfigAction(mcs2)
 			f.expectCreateMachineConfigAction(mcs2)
 			f.runFeature(getKeyFromFeatureGate(features, t))
+		})
+	}
+}
+
+func TestBootstrapFeaturesDefault(t *testing.T) {
+	for _, platform := range []configv1.PlatformType{configv1.AWSPlatformType, configv1.NonePlatformType, "unrecognized"} {
+		t.Run(string(platform), func(t *testing.T) {
+
+			cc := newControllerConfig(ctrlcommon.ControllerConfigName, platform)
+			mcp := helpers.NewMachineConfigPool("master", nil, helpers.MasterSelector, "v0")
+			mcp2 := helpers.NewMachineConfigPool("worker", nil, helpers.WorkerSelector, "v0")
+			mcps := []*mcfgv1.MachineConfigPool{mcp, mcp2}
+
+			features := createNewDefaultFeatureGate()
+
+			mcs, err := RunFeatureGateBootstrap("../../../templates", features, cc, mcps)
+			if err != nil {
+				t.Errorf("could not run feature gate bootstrap: %v", err)
+			}
+			if len(mcs) > 0 {
+				t.Errorf("expected no machine config generated with the default feature gate, got %d configs", len(mcs))
+			}
+		})
+	}
+}
+
+func TestBootstrapFeaturesCustomNoUpgrade(t *testing.T) {
+	for _, platform := range []configv1.PlatformType{configv1.AWSPlatformType, configv1.NonePlatformType, "unrecognized"} {
+		t.Run(string(platform), func(t *testing.T) {
+
+			cc := newControllerConfig(ctrlcommon.ControllerConfigName, platform)
+			mcp := helpers.NewMachineConfigPool("master", nil, helpers.MasterSelector, "v0")
+			mcp2 := helpers.NewMachineConfigPool("worker", nil, helpers.WorkerSelector, "v0")
+			mcps := []*mcfgv1.MachineConfigPool{mcp, mcp2}
+
+			features := &osev1.FeatureGate{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: ctrlcommon.ClusterFeatureInstanceName,
+				},
+				Spec: osev1.FeatureGateSpec{
+					FeatureGateSelection: osev1.FeatureGateSelection{
+						FeatureSet: osev1.CustomNoUpgrade,
+						CustomNoUpgrade: &osev1.CustomFeatureGates{
+							Enabled: []string{"CSIMigration"},
+						},
+					},
+				},
+			}
+
+			mcs, err := RunFeatureGateBootstrap("../../../templates", features, cc, mcps)
+			if err != nil {
+				t.Errorf("could not run feature gate bootstrap: %v", err)
+			}
+			if len(mcs) != 2 {
+				t.Errorf("expected 2 machine configs generated with the custom feature gate, got %d configs", len(mcs))
+			}
+
+			for _, mc := range mcs {
+				ignCfg, err := ctrlcommon.ParseAndConvertConfig(mc.Spec.Config.Raw)
+				regfile := ignCfg.Storage.Files[0]
+				conf, err := dataurl.DecodeString(*regfile.Contents.Source)
+				require.NoError(t, err)
+
+				originalKubeConfig, _ := decodeKubeletConfig(conf.Data)
+				defaultFeatureGates, err := generateFeatureMap(createNewDefaultFeatureGate())
+				if err != nil {
+					t.Errorf("could not generate defaultFeatureGates: %v", err)
+				}
+				if reflect.DeepEqual(originalKubeConfig.FeatureGates, *defaultFeatureGates) {
+					t.Errorf("template FeatureGates should not match default openshift/api FeatureGates: (default=%v)", defaultFeatureGates)
+				}
+				if !originalKubeConfig.FeatureGates["CSIMigration"] {
+					t.Errorf("template FeatureGates should contain CSIMigration: %v", originalKubeConfig.FeatureGates)
+				}
+			}
 		})
 	}
 }
