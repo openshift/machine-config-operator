a805bc1fd6724f322985b5b4c9ba19125b968ae7
Denis Moiseev <dmoiseev@redhat.com>
2021-07-09 13:09:59 +0200

    Update library go dependency for out-of-tree providers support on Azure platform

--- vendor/github.com/openshift/library-go/pkg/cloudprovider/external.go
+++ vendor/github.com/openshift/library-go/pkg/cloudprovider/external.go
@@ -17,16 +17,28 @@ const (
 // IsCloudProviderExternal is used to check whether external cloud provider settings should be used in a component.
 // It checks whether the ExternalCloudProvider feature gate is enabled and whether the ExternalCloudProvider feature
 // has been implemented for the platform.
-func IsCloudProviderExternal(platformType configv1.PlatformType, featureGate *configv1.FeatureGate) (bool, error) {
-	switch platformType {
+func IsCloudProviderExternal(platformStatus *configv1.PlatformStatus, featureGate *configv1.FeatureGate) (bool, error) {
+	if platformStatus == nil {
+		return false, fmt.Errorf("platformStatus is required")
+	}
+	switch platformStatus.Type {
 	case configv1.AWSPlatformType,
 		configv1.OpenStackPlatformType:
 		// Platforms that are external based on feature gate presence
+		return isExternalFeatureGateEnabled(featureGate)
+	case configv1.AzurePlatformType:
+		if isAzureStackHub(platformStatus) {
+			return true, nil
+		}
 		return isExternalFeatureGateEnabled(featureGate)
 	default:
 		// Platforms that do not have external cloud providers implemented
 		return false, nil
 	}
+}
+
+func isAzureStackHub(platformStatus *configv1.PlatformStatus) bool {
+	return platformStatus.Azure != nil && platformStatus.Azure.CloudName == configv1.AzureStackCloud
 }
 
 // isExternalFeatureGateEnabled determines whether the ExternalCloudProvider feature gate is present in the current
