7aeacf5780aeb94711aa59c05283d925cb93c846
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2022-04-07 14:03:27 -0500

    Use WaitForLayeredPoolInitialUpdate

--- test/e2e-layering/layering_test.go
+++ test/e2e-layering/layering_test.go
@@ -37,7 +37,7 @@ func (suite *LayeringSuite) SetupSuite() {
 	helpers.LabelAllNodesInPool(suite.T(), cs, "worker", "node-role.kubernetes.io/"+ctrlcommon.ExperimentalLayeringPoolName)
 	helpers.CreateMCP(suite.T(), cs, ctrlcommon.ExperimentalLayeringPoolName)
 
-	// TODO wait for that to rollout and assert that it rebased
+	helpers.WaitForLayeredPoolInitialUpdate(suite.T(), cs, ctrlcommon.ExperimentalLayeringPoolName)
 }
 
 func (suite *LayeringSuite) TestNoReboot() {
