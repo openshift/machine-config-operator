3ef91bbe3775f861d6e761171685c0c1ef173c36
John Kyros <79665180+jkyros@users.noreply.github.com>
2022-04-19 18:28:36 -0500

    Add structures defining pool-owned build resources
    We need to specify what resources the pool should be creating/owning in
    order to manage builds and the relationships between them (triggers,
    which buildconfigs should source from which streams, etc). The pool
    needs some template/plan to follow when it gets triggered to create and
    verify that its resources exist.
    
    This is my not-quite-as-elegant-as-I-had-hoped attempt at that.

---
+++ pkg/controller/build/pool_build_resources.go
@@ -0,0 +1,91 @@
+package build
+
+// This was arose from the need to keep track of resources the pool should ensure the presence of.
+// It is not as elegant as it should be, but the problem I am trying to solve is capturing:
+// 1.) what resources the pool should ensure the presence of and
+// 2.) the relationships between them
+
+import (
+	"bytes"
+	"text/template"
+
+	mcfgv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
+	ctrlcommon "github.com/openshift/machine-config-operator/pkg/controller/common"
+)
+
+type PoolResourceNames struct {
+	ImageStream PoolImageStreamList
+	BuildConfig PoolBuildConfigList
+}
+
+type PoolImageStreamList struct {
+	Base           string
+	RenderedConfig string
+	Content        string
+	CustomContent  string
+	External       string
+	PerNode        string
+}
+
+type PoolBuildConfigList struct {
+	Content       PoolBuildConfig
+	CustomContent PoolBuildConfig
+}
+
+type PoolBuildConfig struct {
+	Name               string
+	Source             string
+	Target             string
+	TriggeredByStreams []string
+	DockerfileContent  string
+}
+
+func PoolBuildResources(pool *mcfgv1.MachineConfigPool) *PoolResourceNames {
+
+	pisl := PoolImageStreamList{
+		Base:           pool.Name + ctrlcommon.ImageStreamSuffixCoreOS,
+		RenderedConfig: pool.Name + ctrlcommon.ImageStreamSuffixRenderedConfig,
+		Content:        pool.Name + ctrlcommon.ImageStreamSuffixMCOContent,
+		CustomContent:  pool.Name + ctrlcommon.ImageStreamSuffixMCOContentCustom,
+		External:       pool.Name + ctrlcommon.ImageStreamSuffixMCOContentExternal,
+		PerNode:        pool.Name + ctrlcommon.ImageStreamSuffixMCOContentPerNode,
+	}
+
+	// Templated dockerfile for the complicated mco-content buildconfig that applies the rendered-config
+	t, _ := template.New(machineConfigContentDockerfile).Parse(machineConfigContentDockerfile)
+	var tpl bytes.Buffer
+	t.Execute(&tpl, pisl)
+
+	bcl := PoolBuildConfigList{
+		Content: PoolBuildConfig{
+			Name:               pool.Name + "-build" + ctrlcommon.ImageStreamSuffixMCOContent,
+			Source:             pisl.Base,
+			Target:             pisl.Content,
+			TriggeredByStreams: []string{pisl.RenderedConfig + ":latest"},
+			DockerfileContent:  tpl.String(),
+		},
+		CustomContent: PoolBuildConfig{
+			Name:               pool.Name + "-build" + ctrlcommon.ImageStreamSuffixMCOContentCustom,
+			Source:             pisl.Content,
+			Target:             pisl.CustomContent,
+			TriggeredByStreams: []string{},
+			DockerfileContent:  dummyDockerfile,
+		},
+	}
+
+	return &PoolResourceNames{pisl, bcl}
+}
+
+// IsManagedImageStream tells us if a given image stream name is one of the names we think we should be managing. This is used to tell if
+// someone has assigned some completely unmanaged imagestream to our layered pool.
+func (prn *PoolResourceNames) IsManagedImageStream(imageStreamName string) bool {
+	// TODO(jkyros): The longer this goes on, the more I feel this should be a map
+	if imageStreamName == prn.ImageStream.Base ||
+		imageStreamName == prn.ImageStream.Content ||
+		imageStreamName == prn.ImageStream.CustomContent ||
+		imageStreamName == prn.ImageStream.External ||
+		imageStreamName == prn.ImageStream.PerNode {
+		return true
+	}
+	return false
+}
