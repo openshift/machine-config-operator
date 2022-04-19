dfdbb762e927629b1a28b2a6006c848aa71b9e84
Qi Wang <qiwan@redhat.com>
2022-04-19 08:38:05 -0400

    Bug 2070285: kcfg controller remove cleanupmc
    Remove cleanUpDuplicatedMC() from pkg/controller/kubelet-config/kubelet_config_controller_test.go
    cleanUpDuplicatedMC() was a symptomatic fix for https://bugzilla.redhat.com/show_bug.cgi?id=1955517.
    The root cause is getManagedKubeletConfigKey(). After https://github.com/openshift/machine-config-operator/commit/b0139eaf9c1f2c4c225e6019e0b14e70ea6f199f  and https://github.com/openshift/machine-config-operator/commit/6b5bdaf1f4fae0eee6630dbb6ca3b6c01f287c52
    bz 1955517 should be fixed. cleanUpDuplicatedMC() is nolonger needed.
    
    I confirmed bz 1955517 is not reproducible without cleanUpDuplicatedMC():
    1.Launch a cluster
    2.Apply 2 kubeletconfigs: one for master pool, one for worker pool.
    3.Upgrade the cluster to a release-payload built from this patch
    4.Should not see degraded 99-xxx-generated-kubelet
    
    Signed-off-by: Qi Wang <qiwan@redhat.com>

--- pkg/controller/kubelet-config/kubelet_config_controller_test.go
+++ pkg/controller/kubelet-config/kubelet_config_controller_test.go
@@ -1,7 +1,6 @@
 package kubeletconfig
 
 import (
-	"context"
 	"fmt"
 	"reflect"
 	"testing"
@@ -31,7 +30,6 @@ import (
 	ctrlcommon "github.com/openshift/machine-config-operator/pkg/controller/common"
 	"github.com/openshift/machine-config-operator/pkg/generated/clientset/versioned/fake"
 	informers "github.com/openshift/machine-config-operator/pkg/generated/informers/externalversions"
-	"github.com/openshift/machine-config-operator/pkg/version"
 	"github.com/openshift/machine-config-operator/test/helpers"
 )
 
@@ -836,61 +834,6 @@ func TestCleanUpStatusConditions(t *testing.T) {
 			}
 		}
 	}
-}
-
-// TestCleanUpDuplicatedMC test the function removes the MC from the MC list
-// if the MC is of old version.
-func TestCleanUpDuplicatedMC(t *testing.T) {
-	v := version.Hash
-	version.Hash = "3.2.0"
-	versionDegrade := "3.1.0"
-	defer func() {
-		version.Hash = v
-	}()
-	f := newFixture(t)
-	ctrl := f.newController()
-	// wrong version needs to be removed
-	machineConfigDegrade := mcfgv1.MachineConfig{
-		ObjectMeta: metav1.ObjectMeta{Name: "generated-kubelet", UID: types.UID(utilrand.String(5))},
-	}
-	machineConfigDegrade.Annotations = make(map[string]string)
-	machineConfigDegrade.Annotations[ctrlcommon.GeneratedByControllerVersionAnnotationKey] = versionDegrade
-	ctrl.client.MachineconfigurationV1().MachineConfigs().Create(context.TODO(), &machineConfigDegrade, metav1.CreateOptions{})
-
-	// not generated machine config should stay
-	machineConfigDegradeNotGen := mcfgv1.MachineConfig{
-		ObjectMeta: metav1.ObjectMeta{Name: "custom-kubelet", UID: types.UID(utilrand.String(5))},
-	}
-	machineConfigDegradeNotGen.Annotations = make(map[string]string)
-	machineConfigDegradeNotGen.Annotations[ctrlcommon.GeneratedByControllerVersionAnnotationKey] = versionDegrade
-	ctrl.client.MachineconfigurationV1().MachineConfigs().Create(context.TODO(), &machineConfigDegradeNotGen, metav1.CreateOptions{})
-
-	// upgraded MC
-	machineConfigUpgrade := mcfgv1.MachineConfig{
-		ObjectMeta: metav1.ObjectMeta{Name: "generated-kubelet-1", UID: types.UID(utilrand.String(5))},
-	}
-	machineConfigUpgrade.Annotations = make(map[string]string)
-	machineConfigUpgrade.Annotations[ctrlcommon.GeneratedByControllerVersionAnnotationKey] = version.Hash
-	ctrl.client.MachineconfigurationV1().MachineConfigs().Create(context.TODO(), &machineConfigUpgrade, metav1.CreateOptions{})
-
-	mcList, err := ctrl.client.MachineconfigurationV1().MachineConfigs().List(context.TODO(), metav1.ListOptions{})
-	require.NoError(t, err)
-	require.Len(t, mcList.Items, 3)
-
-	ctrl.cleanUpDuplicatedMC()
-	// successful test: ony custom and upgraded MCs stay
-	mcList, err = ctrl.client.MachineconfigurationV1().MachineConfigs().List(context.TODO(), metav1.ListOptions{})
-	require.NoError(t, err)
-	require.Len(t, mcList.Items, 2)
-	actual := make(map[string]mcfgv1.MachineConfig)
-	for _, mc := range mcList.Items {
-		require.GreaterOrEqual(t, len(mc.Annotations), 1)
-		actual[mc.Name] = mc
-	}
-	_, ok := actual[machineConfigDegradeNotGen.Name]
-	require.True(t, ok, "expect custom-kubelet in the list, but got false")
-	_, ok = actual[machineConfigUpgrade.Name]
-	require.True(t, ok, "expect generated-kubelet-1 in the list, but got false")
 }
 
 func TestKubeletConfigResync(t *testing.T) {
