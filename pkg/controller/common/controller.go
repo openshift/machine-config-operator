e4101b0359b693f90ab0f6c617ae29f353913846
John Kyros <79665180+jkyros@users.noreply.github.com>
2022-06-15 12:56:28 -0500

    add Name() to controller interface definition
    When looping through controllers, like we do in our controller start
    functions in start.go, it is nice to know which controller is being
    started.
    
    Rather than change the familiar slice/append layout we've grown so used
    to (and keep the controllers in say, a map or something indexed by name,
    I figured it would be better to have each operator know it's own name
    so we can retrieve it.

--- pkg/controller/common/controller.go
+++ pkg/controller/common/controller.go
@@ -1,6 +1,9 @@
 package common
 
+import "context"
+
 // Controller is the common interface all controllers implement
 type Controller interface {
-	Run(workers int, stopCh <-chan struct{})
+	Run(ctx context.Context, workers int)
+	Name() string
 }
