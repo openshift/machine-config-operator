1ac1bce9106c9973a0473042a0117b0a567e26d3
John Kyros <79665180+jkyros@users.noreply.github.com>
2022-06-15 13:33:32 -0500

    Gracefully shutdown controller and release lease
    This:
    - adds main/lease contexts to the controller
    - sets up a counter and channels to track goroutine completion
    - sets up a signal handler to catch when the controller is being
      terminated so we can cancel our contexts
    - gracefully shuts down the controller upon receipt of a SIGINT/SIGTERM
    
    The reason this does not use sync.WaitGroup instead is that
    sync.WaitGroup has no awareness of 'what' it's waiting for, just 'how
    many', so the channels are more useful.
    
    Cribbed off of what the CVO did here:
    https://github.com/openshift/cluster-version-operator/pull/424

--- cmd/machine-config-controller/start.go
+++ cmd/machine-config-controller/start.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/golang/glog"
 	"github.com/openshift/machine-config-operator/cmd/common"
@@ -18,6 +20,7 @@ import (
 	"github.com/openshift/machine-config-operator/pkg/version"
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/tools/leaderelection"
 )
 
@@ -44,9 +47,19 @@ func init() {
 	startCmd.PersistentFlags().StringVar(&startOpts.promMetricsListenAddress, "metrics-listen-address", "127.0.0.1:8797", "Listen address for prometheus metrics listener")
 }
 
+type asyncResult struct {
+	name  string
+	error error
+}
+
 func runStartCmd(cmd *cobra.Command, args []string) {
 	flag.Set("logtostderr", "true")
 	flag.Parse()
+
+	// This is the context that signals whether the operator should be running and doing work
+	runContext, runCancel := context.WithCancel(context.Background())
+	// This is the context that signals whether we should release our leader lease
+	leaderContext, leaderCancel := context.WithCancel(context.Background())
 
 	// To help debugging, immediately log version
 	glog.Infof("Version: %+v (%s)", version.Raw, version.Hash)
@@ -55,18 +68,29 @@ func runStartCmd(cmd *cobra.Command, args []string) {
 	if err != nil {
 		ctrlcommon.WriteTerminationError(fmt.Errorf("creating clients: %w", err))
 	}
-	run := func(ctx context.Context) {
-		ctrlctx := ctrlcommon.CreateControllerContext(cb, ctx.Done(), componentName)
+
+	resultChannel := make(chan asyncResult, 1)
+	resultChannelCount := 0
+
+	// The context that gets passed into this function by the leaderelection stuff is the leaderContext, so we don't use it.
+	// In the one case where we would have used it ( where we've lost a leaderelection for some reason) we really just need to insta-die
+	// rather than cleanly shut down.
+	run := func(_ context.Context) {
+
+		// Set up the signal handler
+		go common.SignalHandler(runCancel)
+
+		ctrlctx := ctrlcommon.CreateControllerContext(cb, runContext.Done(), componentName)
 
 		// Start the metrics handler
-		go ctrlcommon.StartMetricsListener(startOpts.promMetricsListenAddress, ctrlctx.Stop)
+		resultChannelCount++
+		go func() {
+			defer utilruntime.HandleCrash()
+			err := ctrlcommon.StartMetricsListener(startOpts.promMetricsListenAddress, ctrlctx.Stop)
+			resultChannel <- asyncResult{name: "metrics handler", error: err}
+		}()
 
 		controllers := createControllers(ctrlctx)
-		draincontroller := drain.New(
-			ctrlctx.KubeInformerFactory.Core().V1().Nodes(),
-			ctrlctx.ClientBuilder.KubeClientOrDie("node-update-controller"),
-			ctrlctx.ClientBuilder.MachineConfigClientOrDie("node-update-controller"),
-		)
 
 		// Start the shared factory informers that you need to use in your controller
 		ctrlctx.InformerFactory.Start(ctrlctx.Stop)
@@ -75,27 +99,81 @@ func runStartCmd(cmd *cobra.Command, args []string) {
 		ctrlctx.ConfigInformerFactory.Start(ctrlctx.Stop)
 		ctrlctx.OperatorInformerFactory.Start(ctrlctx.Stop)
 
+		// Make sure the informers have started
 		close(ctrlctx.InformersStarted)
 
-		for _, c := range controllers {
-			go c.Run(2, ctrlctx.Stop)
+		// Start the actual controllers
+		for num := range controllers {
+			resultChannelCount++
+			glog.Infof("Starring %s controller", controllers[num].Name())
+			// Closure, need to make sure we don't grab the loop reference
+			controller := controllers[num]
+			go func() {
+				defer utilruntime.HandleCrash()
+				if controller.Name() == "DrainController" {
+					controller.Run(runContext, 5)
+				} else {
+					controller.Run(runContext, 2)
+				}
+				resultChannel <- asyncResult{name: controller.Name() + " controller", error: err}
+			}()
 		}
-		go draincontroller.Run(5, ctrlctx.Stop)
 
-		select {}
+		var shutdownTimer *time.Timer
+		for resultChannelCount > 0 {
+			glog.Infof("Waiting on %d outstanding goroutines.", resultChannelCount)
+			if shutdownTimer == nil { // running
+				select {
+				case <-runContext.Done():
+					glog.Info("Run context completed; beginning two-minute graceful shutdown period.")
+					shutdownTimer = time.NewTimer(2 * time.Minute)
+
+				case result := <-resultChannel:
+					// TODO(jkyros): one of our goroutines puked early, this means we shut down everything.
+					resultChannelCount--
+					if result.error == nil {
+						glog.Infof("Collected %s goroutine.", result.name)
+					} else {
+						glog.Errorf("Collected %s goroutine: %v", result.name, result.error)
+						runCancel() // this will cause shutdownTimer initialization in the next loop
+					}
+				}
+			} else { // shutting down
+				select {
+				case <-shutdownTimer.C: // never triggers after the channel is stopped, although it would not matter much if it did because subsequent cancel calls do nothing.
+					leaderCancel()
+					shutdownTimer.Stop()
+				case result := <-resultChannel:
+					resultChannelCount--
+					if result.error == nil {
+						glog.Infof("Collected %s goroutine.", result.name)
+					} else {
+						glog.Errorf("Collected %s goroutine: %v", result.name, result.error)
+					}
+					if resultChannelCount == 0 {
+						glog.Info("That was the last one, cancelling the leader lease.")
+
+					}
+				}
+			}
+		}
+		leaderCancel()
+		glog.Info("Finished collecting operator goroutines.")
 	}
 
-	leaderElectionCfg := common.GetLeaderElectionConfig(cb.GetBuilderConfig())
+	leaderElectionCfg := common.GetLeaderElectionConfig(runContext, cb.GetBuilderConfig())
 
-	leaderelection.RunOrDie(context.TODO(), leaderelection.LeaderElectionConfig{
-		Lock:          common.CreateResourceLock(cb, startOpts.resourceLockNamespace, componentName),
-		LeaseDuration: leaderElectionCfg.LeaseDuration.Duration,
-		RenewDeadline: leaderElectionCfg.RenewDeadline.Duration,
-		RetryPeriod:   leaderElectionCfg.RetryPeriod.Duration,
+	leaderelection.RunOrDie(leaderContext, leaderelection.LeaderElectionConfig{
+		Lock:            common.CreateResourceLock(cb, startOpts.resourceLockNamespace, componentName),
+		ReleaseOnCancel: true,
+		LeaseDuration:   leaderElectionCfg.LeaseDuration.Duration,
+		RenewDeadline:   leaderElectionCfg.RenewDeadline.Duration,
+		RetryPeriod:     leaderElectionCfg.RetryPeriod.Duration,
 		Callbacks: leaderelection.LeaderCallbacks{
 			OnStartedLeading: run,
 			OnStoppedLeading: func() {
-				glog.Fatalf("leaderelection lost")
+				glog.Infof("Stopped leading. Terminating.")
+				os.Exit(0)
 			},
 		},
 	})
@@ -157,6 +235,12 @@ func createControllers(ctx *ctrlcommon.ControllerContext) []ctrlcommon.Controlle
 			ctx.InformerFactory.Machineconfiguration().V1().MachineConfigPools(),
 			ctx.KubeInformerFactory.Core().V1().Nodes(),
 			ctx.ConfigInformerFactory.Config().V1().Schedulers(),
+			ctx.ClientBuilder.KubeClientOrDie("node-update-controller"),
+			ctx.ClientBuilder.MachineConfigClientOrDie("node-update-controller"),
+		),
+		// The drain controller drains pods from nodes
+		drain.New(
+			ctx.KubeInformerFactory.Core().V1().Nodes(),
 			ctx.ClientBuilder.KubeClientOrDie("node-update-controller"),
 			ctx.ClientBuilder.MachineConfigClientOrDie("node-update-controller"),
 		),
