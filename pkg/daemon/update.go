openshift/machine-config-operator

commit 343475a40a3fe17f725ef4415333379ef3fd621b

Extract extensions even when only updating OS
Somewhere in there the mcDiff.OSUpdate check got removed, and it's fine
the first time you add extensions/change kernel type. If, however, after
that, you update the OS without changing any of those things, rpm-ostree
will still check for the repo file, when it does check you will get a
"No repos found" error, and you will eventually fail and emit an
MCDPivotError.

This re-adds the osUpdate check so that even though it looks like we
aren't making changes to extensions or kernels, the repo file is still
there for rpm-ostree to use/discover/not fail on.

pkg/daemon/update.go
@@ -2179,7 +2179,7 @@ func (dn *CoreOSDaemon) applyLayeredOSChanges(mcDiff machineConfigDiff, oldConfi
 
 	var osExtensionsContentDir string
 	var err error
-	if mcDiff.extensions || mcDiff.kernelType {
+	if mcDiff.osUpdate || mcDiff.extensions || mcDiff.kernelType {
 
 		// TODO(jkyros): the original intent was that we use the extensions container as a service, but that currently results
 		// in a lot of complexity due to boostrap and firstboot where the service isn't easily available, so for now we are going

commit d0665e17946ed0d7d72adee0078bfcb5ef395776

daemon: Only try to apply layered changes if BaseOSExtensionsContainerImage is not empty
In OKD, the BaseOSExtensionsContainerImage image reference does not
exist. The change from https://github.com/openshift/machine-config-operator/commit/343475a40a3fe17f725ef4415333379ef3fd621b
broke OKD installations by trying to always extract the extensions from
the image during the initial rebase.

Fix this by making the daemon only try to extract the extensions when
the image-reference in not empty.

pkg/daemon/update.go
@@ -2180,7 +2180,7 @@ func (dn *CoreOSDaemon) applyLayeredOSChanges(mcDiff machineConfigDiff, oldConfi
 
 	var osExtensionsContentDir string
 	var err error
-	if mcDiff.osUpdate || mcDiff.extensions || mcDiff.kernelType {
+	if newConfig.Spec.BaseOSExtensionsContainerImage != "" && (mcDiff.osUpdate || mcDiff.extensions || mcDiff.kernelType) {
 
 		// TODO(jkyros): the original intent was that we use the extensions container as a service, but that currently results
 		// in a lot of complexity due to boostrap and firstboot where the service isn't easily available, so for now we are going