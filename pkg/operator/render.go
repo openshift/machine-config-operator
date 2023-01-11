4f4c4e051db7407ee9e6fe9e58aa1da289bc79b8
Zack Zlotnik <zzlotnik@redhat.com>
2023-01-11 12:35:07 -0500

    use common template rendering funcs

--- pkg/operator/render.go
+++ pkg/operator/render.go
@@ -8,12 +8,13 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/Masterminds/sprig"
 	"github.com/apparentlymart/go-cidr/cidr"
 	"github.com/ghodss/yaml"
 	"github.com/golang/glog"
 
 	configv1 "github.com/openshift/api/config/v1"
+
+	ctrlcommon "github.com/openshift/machine-config-operator/pkg/controller/common"
 
 	"github.com/openshift/machine-config-operator/manifests"
 	mcfgv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
@@ -57,7 +58,7 @@ func (a *assetRenderer) read() error {
 }
 
 func (a *assetRenderer) addTemplateFuncs() {
-	funcs := sprig.TxtFuncMap()
+	funcs := ctrlcommon.GetTemplateFuncMap()
 	funcs["toYAML"] = toYAML
 	funcs["onPremPlatformAPIServerInternalIP"] = onPremPlatformAPIServerInternalIP
 	funcs["onPremPlatformAPIServerInternalIPs"] = onPremPlatformAPIServerInternalIPs
