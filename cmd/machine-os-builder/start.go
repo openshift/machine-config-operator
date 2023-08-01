6f70abe56ad4a0f9e22af39148bd04fb4e51f7ce
Dalia Khater <dkhater@redhat.com>
2023-08-01 11:51:02 -0500

    Revert "Merge pull request #3830 from dgoodwin/revert-3763-mco-565b"
    This reverts commit 6b2fa8dc0973c383c258c0b687d697bd8ebc8826, reversing
    changes made to 8e61b30333898a6e92c4a94f8bd89dc6804392d2.

---
+++ cmd/machine-os-builder/start.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/openshift/machine-config-operator/pkg/version"
+	"github.com/spf13/cobra"
+	"k8s.io/klog/v2"
+)
+
+var (
+	startCmd = &cobra.Command{
+		Use:   "start",
+		Short: "Starts Machine OS Builder",
+		Long:  "",
+		Run:   runStartCmd,
+	}
+
+	startOpts struct {
+		kubeconfig string
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+	startCmd.PersistentFlags().StringVar(&startOpts.kubeconfig, "kubeconfig", "", "Kubeconfig file to access a remote cluster (testing only)")
+}
+
+func runStartCmd(_ *cobra.Command, _ []string) {
+	flag.Set("logtostderr", "true")
+	flag.Parse()
+
+	klog.V(2).Infof("Options parsed: %+v", startOpts)
+
+	// To help debugging, immediately log version
+	klog.Infof("Version: %+v (%s)", version.Raw, version.Hash)
+
+}
