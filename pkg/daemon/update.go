d94dbb0633aaacc2f1c44956bcd98e7eaa057068
Ben Nemec <bnemec@redhat.com>
2023-08-07 14:35:42 -0500

    Add nmstate-configuration service
    Adds a service to apply NMState configs early in the boot process.
    If the new service applies changes we will skip running configure-ovs
    since br-ex should have been included in the manual configuration.
    
    Also makes the /etc/nmstate directory a reboot-less path so when we
    make changes to it while scaling out new nodes it won't cause all
    of the existing nodes to reboot. The files there are only applied
    once on initial boot anyway so there's no need to reboot for changes.

--- pkg/daemon/update.go
+++ pkg/daemon/update.go
@@ -572,6 +572,12 @@ func calculatePostConfigChangeActionFromMCDiffs(diffFileSet []string) (actions [
 		imageRegistryAuthFile,
 		"/var/lib/kubelet/config.json",
 	}
+	directoriesPostConfigChangeActionNone := []string{
+		// We probably don't want to use this exact path because NMState is
+		// planning to add a service that applies configs from it too, and we
+		// want to make sure our service is the only one processing the configs.
+		"/etc/nmstate",
+	}
 	filesPostConfigChangeActionReloadCrio := []string{
 		constants.ContainerRegistryConfPath,
 		GPGNoRebootPath,
@@ -585,8 +591,13 @@ func calculatePostConfigChangeActionFromMCDiffs(diffFileSet []string) (actions [
 	}
 
 	actions = []string{postConfigChangeActionNone}
-
+path:
 	for _, path := range diffFileSet {
+		for _, dir := range directoriesPostConfigChangeActionNone {
+			if strings.HasPrefix(path, dir) {
+				continue path
+			}
+		}
 		if ctrlcommon.InSlice(path, filesPostConfigChangeActionNone) {
 			continue
 		} else if ctrlcommon.InSlice(path, filesPostConfigChangeActionReloadCrio) {
