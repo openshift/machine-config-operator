e76ec60e7ad219577a758791e355cf59b08078b4
Charlie Doern <cdoern@redhat.com>
2024-02-21 11:47:57 -0500

    ocb-api
    Signed-off-by: Charlie Doern <cdoern@redhat.com>

--- cmd/machine-config-controller/start.go
+++ cmd/machine-config-controller/start.go
@@ -195,6 +195,7 @@ func createControllers(ctx *ctrlcommon.ControllerContext) []ctrlcommon.Controlle
 			ctx.InformerFactory.Machineconfiguration().V1().MachineConfigPools(),
 			ctx.KubeInformerFactory.Core().V1().Nodes(),
 			ctx.KubeInformerFactory.Core().V1().Pods(),
+			ctx.InformerFactory.Machineconfiguration().V1alpha1().MachineOSBuilds(),
 			ctx.ConfigInformerFactory.Config().V1().Schedulers(),
 			ctx.ClientBuilder.KubeClientOrDie("node-update-controller"),
 			ctx.ClientBuilder.MachineConfigClientOrDie("node-update-controller"),
