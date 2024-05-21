2350fce95fc849f635bc251a4238ff214a891433
Dalia Khater <dkhater@redhat.com>
2024-05-21 11:07:25 -0500

    Fix for adding cert to controller config

--- lib/resourceapply/machineconfig.go
+++ lib/resourceapply/machineconfig.go
@@ -13,6 +13,7 @@ import (
 	mcoResourceMerge "github.com/openshift/machine-config-operator/lib/resourcemerge"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog/v2"
 )
 
 // ApplyMachineConfig applies the required machineconfig to the cluster.
@@ -80,21 +81,32 @@ func ApplyMachineConfigNode(client mcfgclientalphav1.MachineConfigNodesGetter, r
 
 // ApplyControllerConfig applies the required machineconfig to the cluster.
 func ApplyControllerConfig(client mcfgclientv1.ControllerConfigsGetter, required *mcfgv1.ControllerConfig) (*mcfgv1.ControllerConfig, bool, error) {
+	klog.V(4).Infof("Getting existing ControllerConfig with name: %s", required.GetName())
 	existing, err := client.ControllerConfigs().Get(context.TODO(), required.GetName(), metav1.GetOptions{})
 	if apierrors.IsNotFound(err) {
+		klog.Info("ControllerConfig not found, creating new one")
 		actual, err := client.ControllerConfigs().Create(context.TODO(), required, metav1.CreateOptions{})
+		if err != nil {
+			klog.Errorf("Failed to create ControllerConfig: %v", err)
+		}
 		return actual, true, err
 	}
 	if err != nil {
+		klog.Errorf("Error fetching ControllerConfig: %v", err)
 		return nil, false, err
 	}
 
 	modified := resourcemerge.BoolPtr(false)
 	mcoResourceMerge.EnsureControllerConfig(modified, existing, *required)
 	if !*modified {
+		klog.V(4).Info("No updates required for the ControllerConfig")
 		return existing, false, nil
 	}
 
+	klog.V(4).Info("Updating existing ControllerConfig")
 	actual, err := client.ControllerConfigs().Update(context.TODO(), existing, metav1.UpdateOptions{})
+	if err != nil {
+		klog.Errorf("Failed to update ControllerConfig: %v", err)
+	}
 	return actual, true, err
 }
