a0c1d84f7e54e96e6a1bcab625b07cbcc4ebb70f
Zack Zlotnik <zzlotnik@redhat.com>
2024-07-11 09:21:41 -0400

    retry build and push operations multiple times

--- pkg/controller/build/assets/wait.sh
+++ pkg/controller/build/assets/wait.sh
@@ -4,12 +4,14 @@
 # within the Build Controller binary (see //go:embed) and injected into a
 # custom build pod.
 
-# Wait until the done file appears.
+# Wait until the digestfile file appears. The presence of this file indicates
+# that the build operation is complete.
 while [ ! -f "/tmp/done/digestfile" ]
 do
 	sleep 1
 done
 
+# Inject the contents of the digestfile into a ConfigMap.
 oc create configmap \
 	"$DIGEST_CONFIGMAP_NAME" \
 	--namespace openshift-machine-config-operator \
