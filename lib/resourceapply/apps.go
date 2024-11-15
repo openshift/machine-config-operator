716da91968dec75892bcabfbc1d5c50dc25a0171
David <djoshy@redhat.com>
2024-11-15 10:22:49 -0500

    operator: add retry on error for apply functions

--- lib/resourceapply/apps.go
+++ lib/resourceapply/apps.go
@@ -2,14 +2,36 @@ package resourceapply
 
 import (
 	"context"
+	"strings"
 
-	"github.com/openshift/library-go/pkg/operator/resource/resourcemerge"
+	"k8s.io/utils/ptr"
+
 	mcoResourceMerge "github.com/openshift/machine-config-operator/lib/resourcemerge"
 	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/klog/v2"
+
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	appsclientv1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 )
+
+func IsApplyErrorRetriable(err error) bool {
+	// Retry on brief rpc errors
+	// See https://issues.redhat.com/browse/OCPBUGS-24228 for more information.
+	// String compare isn't great, but could not find a good error type for this in the standard libraries
+	if strings.Contains(err.Error(), "rpc error") {
+		return true
+	}
+	// Retry on resource conflict errors, i.e "the object has been modified; please apply your changes to the latest version and try again"
+	// See last few comments on https://issues.redhat.com/browse/OCPBUGS-9108 for more information
+	if apierrors.IsConflict(err) {
+		return true
+	}
+	// Add any other errors to be added to the retry here.
+
+	klog.Infof("Skipping retry in Apply fn for error: %s", err)
+	return false
+}
 
 // ApplyDaemonSet applies the required daemonset to the cluster.
 func ApplyDaemonSet(client appsclientv1.DaemonSetsGetter, required *appsv1.DaemonSet) (*appsv1.DaemonSet, bool, error) {
@@ -22,7 +44,7 @@ func ApplyDaemonSet(client appsclientv1.DaemonSetsGetter, required *appsv1.Daemo
 		return nil, false, err
 	}
 
-	modified := resourcemerge.BoolPtr(false)
+	modified := ptr.To(false)
 	mcoResourceMerge.EnsureDaemonSet(modified, existing, *required)
 	if !*modified {
 		return existing, false, nil
@@ -43,7 +65,7 @@ func ApplyDeployment(client appsclientv1.DeploymentsGetter, required *appsv1.Dep
 		return nil, false, err
 	}
 
-	modified := resourcemerge.BoolPtr(false)
+	modified := ptr.To(false)
 	mcoResourceMerge.EnsureDeployment(modified, existing, *required)
 	if !*modified {
 		return existing, false, nil
