783d1d14c8c3553be16aad7b9097dd16b05f64ee
RishabhSaini <rishabhsaini01@gmail.com>
2025-01-29 12:20:06 +0800

    fix paramValye for v1beta1

--- pkg/controller/build/reconciler.go
+++ pkg/controller/build/reconciler.go
@@ -213,12 +213,12 @@ func checkAndInstallPipeline(ctx context.Context, kubeclient clientset.Interface
 	klog.V(2).Infof("TektonConfig is Ready!")
 
 	/*
-	// Ensure Buildah Task exists
-	// Potential problem: ClusterTasks don't exist in V1 API hence the Buildah task may need to be installed into MCO Namespace
-	_, err = tektonclient.TektonV1().Tasks(ctrlcommon.MCONamespace).Get(ctx, tektonClusterTaskName, metav1.GetOptions{})
-	if err != nil {
-		return fmt.Errorf("Error getting ClusterTask Buildah: %v", err)
-	}
+		// Ensure Buildah Task exists
+		// Potential problem: ClusterTasks don't exist in V1 API hence the Buildah task may need to be installed into MCO Namespace
+		_, err = tektonclient.TektonV1().Tasks(ctrlcommon.MCONamespace).Get(ctx, tektonClusterTaskName, metav1.GetOptions{})
+		if err != nil {
+			return fmt.Errorf("Error getting ClusterTask Buildah: %v", err)
+		}
 	*/
 
 	// Ensure Buildah Pipeline exists
@@ -239,36 +239,36 @@ func checkAndInstallPipeline(ctx context.Context, kubeclient clientset.Interface
 			},
 			Spec: tektonv1beta1.PipelineSpec{
 				Params: []tektonv1beta1.ParamSpec{
-					{Name: "logLevel", Type: tektonv1beta1.ParamTypeString, Description: "log level"},
-					{Name: "storageDriver", Type: tektonv1beta1.ParamTypeString, Description: "storage driver"},
-					{Name: "authfile", Type: tektonv1beta1.ParamTypeString, Description: "authfile"},
-					{Name: "tag", Type: tektonv1beta1.ParamTypeString, Description: "Image URL"},
-					{Name: "containerFile", Type: tektonv1beta1.ParamTypeString, Description: "container file"},
-					{Name: "httpProxy", Type: tektonv1beta1.ParamTypeString, Description: "proxy"},
-					{Name: "httpsProxy", Type: tektonv1beta1.ParamTypeString, Description: "proxy"},
-					{Name: "noProxy", Type: tektonv1beta1.ParamTypeString, Description: "proxy"},
-					{Name: "buildContext", Type: tektonv1beta1.ParamTypeString, Description: "context"},
-					{Name: "image", Type: tektonv1beta1.ParamTypeString, Description: "image"},
+					tektonv1beta1.ParamSpec{Name: "logLevel", Type: tektonv1beta1.ParamTypeString, Description: "log level"},
+					tektonv1beta1.ParamSpec{Name: "storageDriver", Type: tektonv1beta1.ParamTypeString, Description: "storage driver"},
+					tektonv1beta1.ParamSpec{Name: "authfile", Type: tektonv1beta1.ParamTypeString, Description: "authfile"},
+					tektonv1beta1.ParamSpec{Name: "tag", Type: tektonv1beta1.ParamTypeString, Description: "Image URL"},
+					tektonv1beta1.ParamSpec{Name: "containerFile", Type: tektonv1beta1.ParamTypeString, Description: "container file"},
+					tektonv1beta1.ParamSpec{Name: "httpProxy", Type: tektonv1beta1.ParamTypeString, Description: "proxy"},
+					tektonv1beta1.ParamSpec{Name: "httpsProxy", Type: tektonv1beta1.ParamTypeString, Description: "proxy"},
+					tektonv1beta1.ParamSpec{Name: "noProxy", Type: tektonv1beta1.ParamTypeString, Description: "proxy"},
+					tektonv1beta1.ParamSpec{Name: "buildContext", Type: tektonv1beta1.ParamTypeString, Description: "context"},
+					tektonv1beta1.ParamSpec{Name: "image", Type: tektonv1beta1.ParamTypeString, Description: "image"},
 				},
 				Results: []tektonv1beta1.PipelineResult{
-					{Name: "IMAGE_DIGEST", Description: "Digest of the image just built", Value: tektonv1beta1.ResultValue{StringVal: "$(tasks.buildah-build.results.IMAGE_DIGEST)"}},
-					{Name: "IMAGE_URL", Description: "Image repository where the built image would be pushed to", Value: tektonv1beta1.ResultValue{StringVal: "$(tasks.buildah-build.results.IMAGE_URL)"}},
+					tektonv1beta1.PipelineResult{Name: "IMAGE_DIGEST", Description: "Digest of the image just built", Value: tektonv1beta1.ResultValue{StringVal: "$(tasks.buildah-build.results.IMAGE_DIGEST)"}},
+					tektonv1beta1.PipelineResult{Name: "IMAGE_URL", Description: "Image repository where the built image would be pushed to", Value: tektonv1beta1.ResultValue{StringVal: "$(tasks.buildah-build.results.IMAGE_URL)"}},
 				},
 				Workspaces: []tektonv1beta1.PipelineWorkspaceDeclaration{
-					{Name: "source"},
+					tektonv1beta1.PipelineWorkspaceDeclaration{Name: "source"},
 				},
 				Tasks: []tektonv1beta1.PipelineTask{
-					{
+					tektonv1beta1.PipelineTask{
 						Name: "prepare-environment",
 						TaskSpec: &tektonv1beta1.EmbeddedTask{
 							TaskSpec: tektonv1beta1.TaskSpec{
 								Workspaces: []tektonv1beta1.WorkspaceDeclaration{
-									{
+									tektonv1beta1.WorkspaceDeclaration{
 										Name: "source",
 									},
 								},
 								Steps: []tektonv1beta1.Step{
-									{
+									tektonv1beta1.Step{
 										Name:   "setup-environment",
 										Image:  "$(params.image)",
 										Script: buildahBuildPipelineScript,
@@ -277,23 +277,23 @@ func checkAndInstallPipeline(ctx context.Context, kubeclient clientset.Interface
 							},
 						},
 					},
-					{
+					tektonv1beta1.PipelineTask{
 						Name: "buildah-build",
 						TaskRef: &tektonv1beta1.TaskRef{
 							Name: tektonClusterTaskName,
 							Kind: tektonv1beta1.ClusterTaskKind,
 						},
 						Params: []tektonv1beta1.Param{
-							{Name: "IMAGE", Value: tektonv1beta1.ParamValue{StringVal: "$(params.tag)"}},
-							{Name: "STORAGE_DRIVER", Value: tektonv1beta1.ParamValue{StringVal: "$(params.storageDriver)"}},
-							{Name: "DOCKERFILE", Value: tektonv1beta1.ParamValue{StringVal: "$(params.containerFile)"}},
-							{Name: "CONTEXT", Value: tektonv1beta1.ParamValue{StringVal: "$(params.buildContext)"}},
-							{Name: "BUILD_EXTRA_ARGS", Value: tektonv1beta1.ParamValue{StringVal: "--authfile=$(params.authfileBuild) --log-level=$(params.logLevel)"}},
-							{Name: "BUILD_ARGS", Value: tektonv1beta1.ParamValue{ArrayVal: []string{"HTTP_PROXY=$(params.httpProxy)", "HTTPS_PROXY=$(params.httpsProxy)", "NO_PROXY=$(params.noProxy)"}}},
-							{Name: "PUSH_EXTRA_ARGS", Value: tektonv1beta1.ParamValue{StringVal: "--authfile=$(params.authfilePush)"}},
+							tektonv1beta1.Param{Name: "IMAGE", Value: tektonv1beta1.ArrayOrString{Type: tektonv1beta1.ParamTypeString, StringVal: "$(params.tag)"}},
+							tektonv1beta1.Param{Name: "STORAGE_DRIVER", Value: tektonv1beta1.ArrayOrString{Type: tektonv1beta1.ParamTypeString, StringVal: "$(params.storageDriver)"}},
+							tektonv1beta1.Param{Name: "DOCKERFILE", Value: tektonv1beta1.ArrayOrString{Type: tektonv1beta1.ParamTypeString, StringVal: "$(params.containerFile)"}},
+							tektonv1beta1.Param{Name: "CONTEXT", Value: tektonv1beta1.ArrayOrString{Type: tektonv1beta1.ParamTypeString, StringVal: "$(params.buildContext)"}},
+							tektonv1beta1.Param{Name: "BUILD_EXTRA_ARGS", Value: tektonv1beta1.ArrayOrString{Type: tektonv1beta1.ParamTypeString, StringVal: "--authfile=$(params.authfileBuild) --log-level=$(params.logLevel)"}},
+							tektonv1beta1.Param{Name: "BUILD_ARGS", Value: tektonv1beta1.ArrayOrString{Type: tektonv1beta1.ParamTypeArray, ArrayVal: []string{"HTTP_PROXY=$(params.httpProxy)", "HTTPS_PROXY=$(params.httpsProxy)", "NO_PROXY=$(params.noProxy)"}}},
+							tektonv1beta1.Param{Name: "PUSH_EXTRA_ARGS", Value: tektonv1beta1.ArrayOrString{Type: tektonv1beta1.ParamTypeString, StringVal: "--authfile=$(params.authfilePush)"}},
 						},
 						Workspaces: []tektonv1beta1.WorkspacePipelineTaskBinding{
-							{Name: "source", Workspace: "source"},
+							tektonv1beta1.WorkspacePipelineTaskBinding{Name: "source", Workspace: "source"},
 						},
 					},
 				},
