7c068d7e6fa5b0a11eec2b528a3a22a933b4e32c
RishabhSaini <rishabhsaini01@gmail.com>
2025-02-03 15:54:05 +0800

    fix DOCKERFILE to path

--- pkg/controller/build/buildrequest/assets/buildah-build-pipeline.sh
+++ pkg/controller/build/buildrequest/assets/buildah-build-pipeline.sh
@@ -12,12 +12,12 @@ export P11_KIT_NO_USER_CONFIG=1
 /usr/bin/p11-kit extract --format=openssl-bundle --filter=certificates --overwrite --comment $DEST/openssl/ca-bundle.trust.crt
 /usr/bin/p11-kit extract --format=pem-bundle --filter=ca-anchors --overwrite --comment --purpose server-auth $DEST/pem/tls-ca-bundle.pem
 
-build_context=$(params.buildContext)
+build_context="$(workspaces.source.path)/$(params.buildContextName)"
 
 # Create a directory to hold our build context.
 mkdir -p "$build_context/machineconfig"
 
 # Copy the Containerfile, Machineconfigs and Additional Trust Bundle from configmaps into our build context.
-echo "$(params.containerFile)" > "$build_context/Containerfile"
+echo "$(params.containerFileData)" > "$build_context/$(params.containerFileName)"
 echo "$(params.machineConfig)" > "$build_context/machineconfig/machineconfig.json.gz"
 cp /etc/pki/ca-trust/source/anchors/openshift-config-user-ca-bundle.crt "$build_context"
