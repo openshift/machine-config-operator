89370a7d1b26f1d72fef3f98750959e9b440eca6
Zack Zlotnik <zzlotnik@redhat.com>
2025-02-13 15:44:08 -0500

    add tmpfiles.d support for usbguard

--- pkg/controller/build/buildrequest/assets/install-extensions.sh
+++ pkg/controller/build/buildrequest/assets/install-extensions.sh
@@ -5,5 +5,10 @@ set -xeuo
 rpm-ostree install "$@"
 
 for pkg in "$@"; do
-	echo "Post-processing action for $pkg"
+	if [[ "$pkg" == "usbguard" ]]; then
+		echo "d /var/log/usbguard 0755 root root -" > /usr/lib/tmpfiles.d/usbguard.conf
+		echo "Created $pkg tmpfiles.d config"
+	else
+		echo "No post-installation action needed for $pkg"
+	fi
 done
