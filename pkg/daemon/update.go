4d9d53dc3b4fceb9ccccdfd8643362bd3da9441e
Isabella Janssen <ijanssen@redhat.com>
2025-03-18 09:10:24 -0400

    daemon: update desired config version in MCN spec in OCL upgrade

--- pkg/daemon/update.go
+++ pkg/daemon/update.go
@@ -837,6 +837,18 @@ func (dn *Daemon) updateOnClusterBuild(oldConfig, newConfig *mcfgv1.MachineConfi
 	oldConfigName := oldConfig.GetName()
 	newConfigName := newConfig.GetName()
 
+	// Add the desired config version to the MCN
+	// 	get MCP associated with node
+	pool, err := helpers.GetPrimaryPoolNameForMCN(dn.mcpLister, dn.node)
+	if err != nil {
+		return err
+	}
+	//  update the MCN spec
+	err = upgrademonitor.GenerateAndApplyMachineConfigNodeSpec(dn.featureGatesAccessor, pool, dn.node, dn.mcfgClient)
+	if err != nil {
+		return fmt.Errorf("error updating MCN spec for node %s: %w", dn.node.Name, err)
+	}
+
 	oldIgnConfig, err := ctrlcommon.ParseAndConvertConfig(oldConfig.Spec.Config.Raw)
 	if err != nil {
 		return fmt.Errorf("parsing old Ignition config failed: %w", err)
