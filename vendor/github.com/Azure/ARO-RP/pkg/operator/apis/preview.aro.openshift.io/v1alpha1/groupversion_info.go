08fbd8ab6d9611ef4362b57ac9e6dd648cc9b1d0
David <djoshy@redhat.com>
2025-07-28 09:02:12 -0400

    vendor bump for ARO API

---
+++ vendor/github.com/Azure/ARO-RP/pkg/operator/apis/preview.aro.openshift.io/v1alpha1/groupversion_info.go
@@ -0,0 +1,26 @@
+// Package v1alpha1 contains API Schema definitions for the aro v1alpha1 API group
+// +kubebuilder:object:generate=true
+// +groupName=preview.aro.openshift.io
+package v1alpha1
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+var (
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: "preview.aro.openshift.io", Version: "v1alpha1"}
+
+	// SchemeGroupVersion manual hack
+	SchemeGroupVersion = GroupVersion
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
