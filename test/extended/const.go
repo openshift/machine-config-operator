ff9045876f402758884e1ed0be25db1d74720c62
David <djoshy@redhat.com>
2025-09-18 09:46:50 -0400

    test/extended: add boot image test helpers

--- test/extended/const.go
+++ test/extended/const.go
@@ -43,4 +43,20 @@ const (
 	TrueString = "True"
 	// FalseString string for false value
 	FalseString = "False"
+
+	// MAPINamespace is the MachineAPI namespace
+	MAPINamespace = "openshift-machine-api"
+
+	// MAPIMachinesetQualifiedName is the fully qualified name of the MAPI MachineSet Resource
+	MAPIMachinesetQualifiedName = "machinesets.machine.openshift.io"
+
+	// GoldenBootImagesConfigMap is the configmap that stores the bootimages refs of the current OCP release
+	GoldenBootImagesConfigMap = "coreos-bootimages"
+
+	// MAPIMasterMachineLabelSelector is the label used to select the control-plane nodes
+	MAPIMasterMachineLabelSelector = "machine.openshift.io/cluster-api-machine-role=master"
+
+	// Labels and Annotations required for determining architecture of a machineset
+	MachineSetArchAnnotationKey = "capacity.cluster-autoscaler.kubernetes.io/labels"
+	ArchLabelKey                = "kubernetes.io/arch="
 )
