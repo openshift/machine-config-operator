9d8970cae2f0962c2efb2b264917a25afd882164
Pablo Rodriguez Nava <pablintino@redhat.com>
2025-09-24 13:19:39 +0200

    MCO-1685: Add mco-sanitize utility main logic

--- devex/cmd/mco-sanitize/main.go
+++ devex/cmd/mco-sanitize/main.go
@@ -1,6 +1,45 @@
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/spf13/cobra"
+	"k8s.io/component-base/cli"
+)
+
 func main() {
-	// Mock function till mco-sanitize fully lands
-	// https://issues.redhat.com/browse/MCO-1685 covers the whole binary development and testing
+	var inputPath string
+	var workerCount int
+
+	rootCmd := &cobra.Command{
+		Use:   "mco-sanitize",
+		Short: "Removes MCO sensitive information from a must-gather report",
+		Long:  "",
+		RunE: func(_ *cobra.Command, _ []string) error {
+			ctx, cancelFn := signal.NotifyContext(
+				context.Background(),
+				os.Interrupt,
+				syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT,
+			)
+			defer cancelFn()
+			config, err := GetConfig()
+			if err != nil {
+				return err
+			}
+			if err := sanitize(ctx, inputPath, workerCount, config); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+
+	rootCmd.PersistentFlags().StringVar(&inputPath, "input", "", "Path to the must-gather directory.")
+	rootCmd.PersistentFlags().IntVar(&workerCount, "workers", runtime.NumCPU(), "Worker count. Defaults to CPU core count.")
+	_ = rootCmd.MarkPersistentFlagRequired("input")
+
+	os.Exit(cli.Run(rootCmd))
 }
