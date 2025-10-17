1d6bae1db9abe7201a933db176a3a44f6b8268b7
Isabella Janssen <ijanssen@redhat.com>
2025-10-17 10:07:23 -0400

    test/extended: add 3 regression tests for the ImageModeStatusReporting FeatureGate

--- test/extended/node.go
+++ test/extended/node.go
@@ -1,15 +1,20 @@
 package extended
 
 import (
+	"context"
 	"fmt"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/openshift/machine-config-operator/pkg/daemon/constants"
 	exutil "github.com/openshift/machine-config-operator/test/extended/util"
 	logger "github.com/openshift/machine-config-operator/test/extended/util/logext"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/sets"
 
 	o "github.com/onsi/gomega"
@@ -446,4 +451,39 @@ func getReadyNodes(oc *exutil.CLI) (sets.Set[string], error) {
 		node.oc.SetShowInfo()
 	}
 	return nodeSet, nil
+}
+
+// `GetNodesByRoleOriginPort` gets all nodes labeled with the desired role
+// TODO (MCO-1960): Replace this function ported from o/origin with a standardized helper.
+func GetNodesByRoleOriginPort(oc *exutil.CLI, role string) ([]corev1.Node, error) {
+	listOptions := metav1.ListOptions{
+		LabelSelector: labels.SelectorFromSet(labels.Set{fmt.Sprintf("node-role.kubernetes.io/%s", role): ""}).String(),
+	}
+	nodes, err := oc.AsAdmin().KubeClient().CoreV1().Nodes().List(context.TODO(), listOptions)
+	if err != nil {
+		return nil, err
+	}
+	return nodes.Items, nil
+}
+
+// `WaitForNodeCurrentConfigOriginPort` waits up to 5 minutes for a input node to have a current
+// config equal to the `config` parameter
+// TODO (MCO-1960): Replace this function ported from o/origin with a standardized helper.
+func WaitForNodeCurrentConfigOriginPort(oc *exutil.CLI, nodeName, config string) {
+	o.Eventually(func() bool {
+		node, nodeErr := oc.AsAdmin().KubeClient().CoreV1().Nodes().Get(context.TODO(), nodeName, metav1.GetOptions{})
+		if nodeErr != nil {
+			logger.Infof("Failed to get node '%v', error :%v", nodeName, nodeErr)
+			return false
+		}
+
+		// Check if the node's current config matches the input config version
+		nodeCurrentConfig := node.Annotations[constants.CurrentMachineConfigAnnotationKey]
+		if nodeCurrentConfig == config {
+			logger.Infof("Node '%v' has successfully updated and has a current config version of '%v'.", nodeName, nodeCurrentConfig)
+			return true
+		}
+		logger.Infof("Node '%v' has a current config version of '%v'. Waiting for the node's current config version to be '%v'.", nodeName, nodeCurrentConfig, config)
+		return false
+	}, 5*time.Minute, 10*time.Second).Should(o.BeTrue(), "Timed out waiting for node '%v' to have a current config version of '%v'.", nodeName, config)
 }
