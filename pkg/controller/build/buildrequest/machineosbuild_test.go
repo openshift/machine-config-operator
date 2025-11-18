e0db87add595169f02381e3da46b18a446485aac
Zack Zlotnik <zzlotnik@redhat.com>
2025-11-18 10:02:30 -0500

    try this again...

--- pkg/controller/build/buildrequest/machineosbuild_test.go
+++ pkg/controller/build/buildrequest/machineosbuild_test.go
@@ -5,11 +5,13 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 
 	mcfgv1 "github.com/openshift/api/machineconfiguration/v1"
 	"github.com/openshift/machine-config-operator/pkg/controller/build/fixtures"
 	"github.com/openshift/machine-config-operator/pkg/controller/build/utils"
+	ctrlcommon "github.com/openshift/machine-config-operator/pkg/controller/common"
 	testhelpers "github.com/openshift/machine-config-operator/test/helpers"
 )
 
@@ -18,6 +20,21 @@ func TestMachineOSBuild(t *testing.T) {
 	t.Parallel()
 
 	poolName := "worker"
+
+	getMachineConfig := func() *mcfgv1.MachineConfig {
+		return &mcfgv1.MachineConfig{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					ctrlcommon.ReleaseImageVersionAnnotationKey: "release-version",
+				},
+				Name: "rendered-worker-1",
+			},
+			Spec: mcfgv1.MachineConfigSpec{
+				OSImageURL:                     fixtures.BaseOSContainerImage,
+				BaseOSExtensionsContainerImage: fixtures.BaseOSExtensionsContainerImage,
+			},
+		}
+	}
 
 	getMachineOSConfig := func() *mcfgv1.MachineOSConfig {
 		return testhelpers.NewMachineOSConfigBuilder(poolName).WithMachineConfigPool(poolName).MachineOSConfig()
@@ -28,7 +45,7 @@ func TestMachineOSBuild(t *testing.T) {
 	}
 
 	// Some of the test cases expect the hash name to be the same. This is that hash value.
-	expectedCommonHashName := "worker-55592464e51104dcc274a300565fec9e"
+	expectedCommonHashName := "worker-699e6be74658adcb3ff2b48f32cd1584"
 
 	testCases := []struct {
 		name         string
@@ -67,17 +84,14 @@ func TestMachineOSBuild(t *testing.T) {
 				MachineConfigPool: getMachineConfigPool(),
 			},
 		},
-		// These cases ensure that the hashed name remains stable regardless of
-		// which source of truth is used for the base OS image, extensions image,
-		// and / or release version. In these cases, the source of truth can either
-		// be the value from the MachineOSConfig or the OSImageURLConfig struct.
+		// These cases ensure that the hashed name remains stable.
 		{
 			name:         "All values from OSImageURLConfig",
 			expectedName: expectedCommonHashName,
 			opts: MachineOSBuildOpts{
+				MachineConfig:     getMachineConfig(),
 				MachineOSConfig:   getMachineOSConfig(),
 				MachineConfigPool: getMachineConfigPool(),
-				OSImageURLConfig:  fixtures.OSImageURLConfig(),
 			},
 		},
 		// These cases ensure that pausing the MachineConfigPool does not affect the hash.
@@ -85,18 +99,18 @@ func TestMachineOSBuild(t *testing.T) {
 			name:         "Unpaused MachineConfigPool",
 			expectedName: expectedCommonHashName,
 			opts: MachineOSBuildOpts{
+				MachineConfig:     getMachineConfig(),
 				MachineOSConfig:   getMachineOSConfig(),
 				MachineConfigPool: getMachineConfigPool(),
-				OSImageURLConfig:  fixtures.OSImageURLConfig(),
 			},
 		},
 		{
 			name:         "Paused MachineConfigPool",
 			expectedName: expectedCommonHashName,
 			opts: MachineOSBuildOpts{
+				MachineConfig:     getMachineConfig(),
 				MachineOSConfig:   getMachineOSConfig(),
 				MachineConfigPool: testhelpers.NewMachineConfigPoolBuilder(poolName).WithPaused().MachineConfigPool(),
-				OSImageURLConfig:  fixtures.OSImageURLConfig(),
 			},
 		},
 	}
@@ -142,9 +156,20 @@ func TestMachineOSBuildLabelConsistency(t *testing.T) {
 	obj := fixtures.NewObjectsForTest("worker")
 
 	mosb, err := NewMachineOSBuild(MachineOSBuildOpts{
+		MachineConfig: &mcfgv1.MachineConfig{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					ctrlcommon.ReleaseImageVersionAnnotationKey: "release-version",
+				},
+				Name: "rendered-worker-1",
+			},
+			Spec: mcfgv1.MachineConfigSpec{
+				OSImageURL:                     fixtures.BaseOSContainerImage,
+				BaseOSExtensionsContainerImage: fixtures.BaseOSExtensionsContainerImage,
+			},
+		},
 		MachineConfigPool: obj.MachineConfigPool,
 		MachineOSConfig:   obj.MachineOSConfig,
-		OSImageURLConfig:  fixtures.OSImageURLConfig(),
 	})
 
 	assert.NoError(t, err)
