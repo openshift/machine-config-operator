0632e3cc2f83f1e7f98b956160e7ceb1fde6a250
Andrea Fasano <afasano@redhat.com>
2025-12-09 09:47:56 -0500

    add tls certificate to InternalReleaseImage registry
    This patch injects the specific tls certificate generated by the installer into the IRI registry. It also adds the root CA certificate to all the master nodes, to allow pulling the images from other nodes registry

---
+++ pkg/controller/internalreleaseimage/internalreleaseimage_helpers_test.go
@@ -0,0 +1,175 @@
+package internalreleaseimage
+
+// Test builders and helper methods.
+
+import (
+	"testing"
+
+	ign3types "github.com/coreos/ignition/v2/config/v3_5/types"
+	mcfgv1 "github.com/openshift/api/machineconfiguration/v1"
+	mcfgv1alpha1 "github.com/openshift/api/machineconfiguration/v1alpha1"
+	"github.com/openshift/machine-config-operator/pkg/controller/common"
+	ctrlcommon "github.com/openshift/machine-config-operator/pkg/controller/common"
+	templatectrl "github.com/openshift/machine-config-operator/pkg/controller/template"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func verifyInternalReleaseMasterMachineConfig(t *testing.T, mc *mcfgv1.MachineConfig) {
+	assert.Equal(t, iriMachineConfigName, mc.Name)
+	assert.Equal(t, common.MachineConfigPoolMaster, mc.Labels[mcfgv1.MachineConfigRoleLabelKey])
+	assert.Equal(t, "InternalReleaseImage", mc.OwnerReferences[0].Kind)
+
+	ignCfg, err := ctrlcommon.ParseAndConvertConfig(mc.Spec.Config.Raw)
+	assert.NoError(t, err)
+
+	assert.Len(t, ignCfg.Systemd.Units, 1)
+	assert.Contains(t, *ignCfg.Systemd.Units[0].Contents, "docker-registry-image-pullspec")
+
+	assert.Len(t, ignCfg.Storage.Files, 3)
+	verifyIgnitionFile(t, &ignCfg, "/etc/pki/ca-trust/source/anchors/root-ca.crt", "root-ca-data")
+	verifyIgnitionFile(t, &ignCfg, "/etc/iri-registry/certs/tls.key", "iri-tls-key")
+	verifyIgnitionFile(t, &ignCfg, "/etc/iri-registry/certs/tls.crt", "iri-tls-crt")
+}
+
+func verifyIgnitionFile(t *testing.T, ignCfg *ign3types.Config, path string, expectedContent string) {
+	data, err := ctrlcommon.GetIgnitionFileDataByPath(ignCfg, path)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedContent, string(data), path)
+}
+
+// objs is an helper func to improve the test readability.
+func objs(builders ...objBuilder) func() []runtime.Object {
+	return func() []runtime.Object {
+		objects := []runtime.Object{}
+		for _, b := range builders {
+			objects = append(objects, b.build())
+		}
+		return objects
+	}
+}
+
+type objBuilder interface {
+	build() runtime.Object
+}
+
+// iriBuilder simplifies the creation of an InternalReleaseImage resource in the test.
+type iriBuilder struct {
+	obj *mcfgv1alpha1.InternalReleaseImage
+}
+
+func iri() *iriBuilder {
+	return &iriBuilder{
+		obj: &mcfgv1alpha1.InternalReleaseImage{
+			ObjectMeta: v1.ObjectMeta{
+				Name: ctrlcommon.InternalReleaseImageInstanceName,
+			},
+		},
+	}
+}
+
+func (ib *iriBuilder) finalizer(f ...string) *iriBuilder {
+	ib.obj.SetFinalizers(f)
+	return ib
+}
+
+func (ib *iriBuilder) setDeletionTimestamp() *iriBuilder {
+	now := v1.Now()
+	ib.obj.SetDeletionTimestamp(&now)
+	return ib
+}
+
+func (ib *iriBuilder) build() runtime.Object {
+	return ib.obj
+}
+
+// controllerConfigBuilder simplifies the creation of a ControllerConfig resource in the test.
+type controllerConfigBuilder struct {
+	obj *mcfgv1.ControllerConfig
+}
+
+func cconfig() *controllerConfigBuilder {
+	return &controllerConfigBuilder{
+		obj: &mcfgv1.ControllerConfig{
+			ObjectMeta: v1.ObjectMeta{
+				Name: ctrlcommon.ControllerConfigName,
+			},
+			Spec: mcfgv1.ControllerConfigSpec{
+				Images: map[string]string{
+					templatectrl.DockerRegistryKey: "docker-registry-image-pullspec",
+				},
+				RootCAData: []byte("root-ca-data"),
+			},
+		},
+	}
+}
+
+func (ccb *controllerConfigBuilder) dockerRegistryImage(image string) *controllerConfigBuilder {
+	ccb.obj.Spec.Images[templatectrl.DockerRegistryKey] = image
+	return ccb
+}
+
+func (ccb *controllerConfigBuilder) build() runtime.Object {
+	return ccb.obj
+}
+
+// machineConfigBuilder simplifies the creation of a MachineConfig resource in the test.
+type machineConfigBuilder struct {
+	obj *mcfgv1.MachineConfig
+}
+
+func mastermachineconfig() *machineConfigBuilder {
+	return &machineConfigBuilder{
+		obj: &mcfgv1.MachineConfig{
+			ObjectMeta: v1.ObjectMeta{
+				Name: iriMachineConfigName,
+				Labels: map[string]string{
+					mcfgv1.MachineConfigRoleLabelKey: common.MachineConfigPoolMaster,
+				},
+				OwnerReferences: []v1.OwnerReference{
+					{
+						Kind: "InternalReleaseImage",
+					},
+				},
+			},
+			Spec: mcfgv1.MachineConfigSpec{
+				Config: runtime.RawExtension{},
+			},
+		},
+	}
+}
+
+func (mcb *machineConfigBuilder) ignition(ign string) *machineConfigBuilder {
+	mcb.obj.Spec.Config.Raw = []byte(ign)
+	return mcb
+}
+
+func (mcb *machineConfigBuilder) build() runtime.Object {
+	return mcb.obj
+}
+
+// secretBuilder simplifies the creation of a Secret resource in the test.
+type secretBuilder struct {
+	obj *corev1.Secret
+}
+
+func iriCertSecret() *secretBuilder {
+	return &secretBuilder{
+		obj: &corev1.Secret{
+			ObjectMeta: v1.ObjectMeta{
+				Namespace: common.MCONamespace,
+				Name:      ctrlcommon.InternalReleaseImageTLSSecretName,
+			},
+			Data: map[string][]byte{
+				"tls.key": []byte("iri-tls-key"),
+				"tls.crt": []byte("iri-tls-crt"),
+			},
+		},
+	}
+}
+
+func (sb *secretBuilder) build() runtime.Object {
+	return sb.obj
+}
