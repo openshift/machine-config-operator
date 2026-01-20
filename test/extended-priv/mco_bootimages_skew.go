75c92ea6f81ec2978d2b9d62713afb484d3ff849
David <djoshy@redhat.com>
2026-01-20 10:39:30 -0500

    bootimage: enable opt-out for azure and vsphere

--- test/extended-priv/mco_bootimages_skew.go
+++ test/extended-priv/mco_bootimages_skew.go
@@ -124,8 +124,8 @@ var _ = g.Describe("[sig-mco][Suite:openshift/machine-config-operator/disruptive
 	})
 
 	g.It("Verify Automatic mode and Upgradeable (Sad Case) [apigroup:machineconfiguration.openshift.io]", func(_ context.Context) {
-		// only applicable on GCP, AWS clusters
-		skipTestIfSupportedPlatformNotMatched(oc, GCPPlatform, AWSPlatform)
+		// only applicable on clustres where we support automatic updates: A
+		skipTestIfSupportedPlatformNotMatched(oc, GCPPlatform, AWSPlatform, AzurePlatform, VspherePlatform)
 
 		// No opinion on skew enforcement for these platforms will result in Automatic mode
 		o.Expect(machineConfiguration.RemoveSkew()).To(o.Succeed())
