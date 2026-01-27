4dbab5a945bf5adecccda8567d423873526dd04c
David <djoshy@redhat.com>
2026-01-27 10:38:05 -0500

    test/extended-priv: account for skew API

--- test/extended-priv/machineconfiguration.go
+++ test/extended-priv/machineconfiguration.go
@@ -82,3 +82,39 @@ func (mc MachineConfiguration) GetAllManagedBootImagesResources() ([]string, err
 	}
 	return strings.Fields(result), nil
 }
+
+// SetManualSkew configures bootImageSkewEnforcement to Manual mode with the specified mode type and version.
+// mode should be "RHCOSVersion" or "OCPVersion", version is the corresponding version string.
+func (mc MachineConfiguration) SetManualSkew(mode, version string) error {
+	logger.Infof("Setting .spec.bootImageSkewEnforcement to Manual mode (%s: %s) on %s", mode, version, mc)
+	var versionField string
+	switch mode {
+	case "RHCOSVersion":
+		versionField = `"rhcosVersion":"` + version + `"`
+	case "OCPVersion":
+		versionField = `"ocpVersion":"` + version + `"`
+	default:
+		versionField = `"rhcosVersion":"` + version + `"`
+	}
+	return mc.Patch("merge", `{"spec":{"bootImageSkewEnforcement":{"mode":"Manual","manual":{"mode":"`+mode+`",`+versionField+`}}}}`)
+}
+
+// SetNoneSkew configures bootImageSkewEnforcement to None mode, effectively disabling skew enforcement
+func (mc MachineConfiguration) SetNoneSkew() error {
+	logger.Infof("Setting .spec.bootImageSkewEnforcement to None mode on %s", mc)
+	return mc.Patch("merge", `{"spec":{"bootImageSkewEnforcement":{"mode":"None"}}}`)
+}
+
+// RemoveSkew removes the bootImageSkewEnforcement config from MachineConfiguration
+func (mc MachineConfiguration) RemoveSkew() error {
+	logger.Infof("Removing .spec.bootImageSkewEnforcement from %s", mc)
+	skewConfig, err := mc.Get(`{.spec.bootImageSkewEnforcement}`)
+	if err != nil {
+		return err
+	}
+	if skewConfig == "" {
+		logger.Infof(".spec.bootImageSkewEnforcement does not exist. No need to remove it")
+		return nil
+	}
+	return mc.Patch("json", `[{ "op": "remove", "path": "/spec/bootImageSkewEnforcement"}]`)
+}
