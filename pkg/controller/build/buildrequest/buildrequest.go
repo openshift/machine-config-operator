d40b2284b25526e7651f2789d528b0e2b8be967a
Zack Zlotnik <zzlotnik@redhat.com>
2026-03-20 16:35:03 -0400

    Revert "extract oc binary from base OS image after build"
    This reverts commit 483c7c000977bd94cbb99f24089b090ebabad2c1.

--- pkg/controller/build/buildrequest/buildrequest.go
+++ pkg/controller/build/buildrequest/buildrequest.go
@@ -451,10 +451,6 @@ func (br buildRequestImpl) toBuildahPod() *corev1.Pod {
 			Name:  "NO_PROXY",
 			Value: noProxy,
 		},
-		{
-			Name:  "BASE_OS_IMAGE_PULLSPEC",
-			Value: br.opts.OSImageURLConfig.BaseOSContainerImage,
-		},
 	}
 
 	securityContext := &corev1.SecurityContext{}
@@ -564,6 +560,7 @@ func (br buildRequestImpl) toBuildahPod() *corev1.Pod {
 			VolumeSource: corev1.VolumeSource{
 				Secret: &corev1.SecretVolumeSource{
 					SecretName: br.getBasePullSecretName(),
+					// SecretName: br.opts.MachineOSConfig.Spec.BuildInputs.BaseImagePullSecret.Name,
 					Items: []corev1.KeyToPath{
 						{
 							Key:  corev1.DockerConfigJsonKey,
@@ -578,6 +575,7 @@ func (br buildRequestImpl) toBuildahPod() *corev1.Pod {
 			Name: "final-image-push-creds",
 			VolumeSource: corev1.VolumeSource{
 				Secret: &corev1.SecretVolumeSource{
+					// SecretName: br.opts.MachineOSConfig.Spec.BuildInputs.RenderedImagePushSecret.Name,
 					SecretName: br.getFinalPushSecretName(),
 					Items: []corev1.KeyToPath{
 						{
@@ -589,11 +587,14 @@ func (br buildRequestImpl) toBuildahPod() *corev1.Pod {
 			},
 		},
 		{
-			// Provides a way for the "image-build" container to pass the digested
-			// pullspec and oc binary to the "create-digest-configmap" container.
+			// Provides a way for the "image-build" container to signal that it
+			// finished so that the "wait-for-done" container can retrieve the
+			// iamge SHA.
 			Name: "done",
 			VolumeSource: corev1.VolumeSource{
-				EmptyDir: &corev1.EmptyDirVolumeSource{},
+				EmptyDir: &corev1.EmptyDirVolumeSource{
+					Medium: corev1.StorageMediumMemory,
+				},
 			},
 		},
 		{
@@ -670,7 +671,7 @@ func (br buildRequestImpl) toBuildahPod() *corev1.Pod {
 					// us to avoid parsing log files.
 					Name:            "create-digest-configmap",
 					Command:         append(command, digestCMScript),
-					Image:           br.opts.Images.MachineConfigOperator,
+					Image:           br.opts.OSImageURLConfig.BaseOSContainerImage,
 					Env:             env,
 					ImagePullPolicy: corev1.PullAlways,
 					SecurityContext: securityContext,
