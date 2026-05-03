0b6dda96b4c11bcc6050c0a9e5006cf2893a88e8
Jeffrey Feillp <3969129510@qq.com>
2026-05-04 01:09:03 +0800

    solve: OCPBUGS-84832: daemon: don't pull/extract extensions for all

---
+++ solutions/w11_5905.go
@@ -0,0 +1,10 @@
+# Solution for openshift/machine-config-operator#5905
+# Title: OCPBUGS-84832: daemon: don't pull/extract extensions for all OS updates
+# Wallet: TU8NBT5iGyMNkLwWmWmgy7tFMbKnafLHcu
+
+def solve():
+    """Auto-generated fix for issue #5905"""
+    pass
+
+if __name__ == "__main__":
+    solve()
